b68e6da9d45cb9f1c1b01bcd3686732065799bf7
Florian Rand <florian.rand@gmail.com>
2021-10-20 17:17:20 +0200

    Header placeholder component

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = (): JSX.Element => {
+  return (
+    <div className="header bg-gray-500 py-8">
+      <div className="nav container px-4">this is the header</div>
+    </div>
+  );
+};
+
+export default Header;
