c0b1f71d08af52820ec23997196bd90b197e55a8
Florian Rand <florian.rand@gmail.com>
2021-10-20 16:11:49 +0200

    Cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config"; // Default Seo configuration options
+import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ChakraProvider>
+    <>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
-    </ChakraProvider>
+    </>
   );
 }
 
