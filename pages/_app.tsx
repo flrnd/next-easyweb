848e08a9d09b798e01188ecaf5eac6798c2aac46
Florian Rand <florian.rand@gmail.com>
2021-10-20 17:16:39 +0200

    Default layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config"; // Default Seo configuration options
 import "../styles/global.css";
+import Layout from "../components/Layout";
+import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <>
+    <Layout>
       <DefaultSeo {...SEO} />
+      <Header />
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
 
