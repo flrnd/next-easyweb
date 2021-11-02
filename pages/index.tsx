7f7cc53475f3d33ba97e18a6222f0b98a6881873
Florian Rand <florian.rand@gmail.com>
2021-11-02 16:24:50 +0100

    Update index with placeholder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import { NextSeo } from "next-seo";
+import Content from "../components/section/Content";
+import Feature from "../components/section/Feature";
 
 export const Index = (): JSX.Element => (
   <>
     <NextSeo title="This is a title" description="this is a description" />
     <main className="bg-red-100 py-6">
-      <h1>Hello there</h1>
-      <p>this is the main section</p>
+      <Feature media="feature">
+        <Content
+          title="This is a title"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          buttonText="more info"
+          buttonURL="/more_info"
+        />
+      </Feature>
     </main>
   </>
 );
