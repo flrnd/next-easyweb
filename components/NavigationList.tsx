d4a1dd7dfb44c1b7fae0fdae50d0a08898511e81
Florian Rand <florian.rand@gmail.com>
2021-11-02 11:17:13 +0100

    Refactor NavigationMenu to NavigationList

---
+++ components/NavigationList.tsx
@@ -0,0 +1,22 @@
+import createNavigationList from "../util/createNavigationList";
+import NavigationListItem from "./NavigationListItem";
+
+interface Props {
+  isVertical?: boolean;
+  list: string[];
+}
+const NavigationList = ({ isVertical = false, list }: Props): JSX.Element => {
+  const navigationList = createNavigationList(list);
+
+  return (
+    <ul className={`flex ${isVertical && "flex-col"}`}>
+      {navigationList.map((navItem) => (
+        <li key={navItem.name}>
+          <NavigationListItem item={navItem} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NavigationList;
