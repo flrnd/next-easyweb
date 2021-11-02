a0e2996277c015d28b9ebddfb422e250d32fefbd
Florian Rand <florian.rand@gmail.com>
2021-11-02 18:07:30 +0100

    Add content component test

---
+++ test/components/section/Content.test.tsx
@@ -0,0 +1,35 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import Content from "../../../components/section/Content";
+import mockUrl from "../../__mocks__/mockUrl";
+
+afterAll(cleanup);
+
+describe("Content", () => {
+  const title = "This is a title";
+  const description =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  const buttonText = "more info";
+  const buttonURL = "/more_info";
+
+  render(
+    <Content
+      title={title}
+      description={description}
+      buttonText={buttonText}
+      buttonURL={buttonURL}
+    />
+  );
+
+  it("should render a heading, description and a button", () => {
+    // heading
+    expect(screen.getByRole("heading").textContent).toBe(title);
+    // description
+    expect(screen.getByText(description).textContent).toBe(description);
+    // button && button link
+    expect(screen.getByRole("button").textContent).toBe(buttonText);
+    const link = screen.getByRole("link", {
+      name: buttonText,
+    }) as HTMLAnchorElement;
+    expect(link.href).toBe(mockUrl(buttonURL));
+  });
+});
