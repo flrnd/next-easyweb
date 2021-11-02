0d810b9d28647895d8edd7cc59644c8c7bb1ffd9
Florian Rand <florian.rand@gmail.com>
2021-11-02 18:14:07 +0100

    Add another placeholder feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,14 @@ export const Index = (): JSX.Element => (
           buttonURL="/more_info"
         />
       </Feature>
+      <Feature isMediaLeft={true} media="feature">
+        <Content
+          title="This is a title"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          buttonText="more info"
+          buttonURL="/more_info"
+        />
+      </Feature>
     </main>
   </>
 );
