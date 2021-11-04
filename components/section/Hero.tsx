bd073473ca837b1a0f7b684c66a27aecba80b040
Florian Rand <florian.rand@gmail.com>
2021-11-04 15:07:30 +0100

    Media is optional, structure and temporal buttons

--- components/section/Hero.tsx
+++ components/section/Hero.tsx
@@ -3,7 +3,7 @@ import Media from "./Media";
 interface Props {
   valueProposition: string;
   valueDescription: string;
-  media: string;
+  media?: string;
 }
 
 const Hero = ({
@@ -13,9 +13,27 @@ const Hero = ({
 }: Props): JSX.Element => {
   return (
     <div className="hero">
-      <h1>{valueProposition}</h1>
+      <h1 className="heading">{valueProposition}</h1>
       <p>{valueDescription}</p>
-      <Media source={media} height={600} width={400} />
+      <div className="mt-8 md:mt-5 flex justify-center">
+        <div className="rounded-md shadow">
+          <a
+            href="#"
+            className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+          >
+            Get started
+          </a>
+        </div>
+        <div className="mt-3 sm:mt-0 sm:ml-3">
+          <a
+            href="#"
+            className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+          >
+            Live demo
+          </a>
+        </div>
+      </div>
+      {media && <Media source={media} height={600} width={600} />}
     </div>
   );
 };
