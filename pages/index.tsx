d9f499cdac9000b410de5fc140a034a06c17317c
Florian Rand <florian.rand@gmail.com>
2021-11-04 15:08:08 +0100

    Add hero component placeholder to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import { NextSeo } from "next-seo";
 import Content from "../components/section/Content";
 import Feature from "../components/section/Feature";
+import Hero from "../components/section/Hero";
 
 export const Index = (): JSX.Element => (
   <>
     <NextSeo title="This is a title" description="this is a description" />
     <main className="bg-red-100 py-6">
+      <Hero
+        valueProposition="Data to enrich your online business"
+        valueDescription="Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua."
+      />
       <Feature media="feature">
         <Content
           title="This is a title"
