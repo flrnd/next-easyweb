fcfb54d10cacc7e8468e50bcfa0220bf69a752d9
Florian Rand <florian.rand@gmail.com>
2021-11-05 08:39:30 +0100

    Add button test case

--- test/components/elements/Button.test.tsx
+++ test/components/elements/Button.test.tsx
@@ -2,16 +2,21 @@ import { render, screen } from "@testing-library/react";
 import Button from "../../../components/elements/Button";
 
 describe("Button", () => {
-  it("should render a button", () => {
+  it("should render a button with link", () => {
+    const baseURL = "http://localhost";
+    const text = "Get Started";
+    const path = "/get-started";
     render(
       <Button
-        text="Get Started"
+        text={text}
         bgColor="bg-red-200"
         textColor="text-white"
-        path="#"
+        path={path}
       />
     );
+    const link = screen.getByRole("link") as HTMLAnchorElement;
 
-    expect(screen.getByRole("link").textContent).toBe("Write a proper test");
+    expect(link.href).toBe(`${baseURL}${path}`);
+    expect(screen.getByRole("link").textContent).toBe(text);
   });
 });
