71894a7f53493f0891baaf8c6417658b259ef317
Florian Rand <florian.rand@gmail.com>
2021-11-05 11:30:04 +0100

    Add heading test cases

--- test/components/elements/Heading.test.tsx
+++ test/components/elements/Heading.test.tsx
@@ -2,9 +2,50 @@ import { render, screen } from "@testing-library/react";
 import Heading from "../../../components/elements/Heading";
 
 describe("Heading", () => {
-  it("should render a heading", () => {
-    render(<Heading level={1}>Heading 1</Heading>);
+  it("should render a heading with its props", () => {
+    const a11yTitle = "a11y Title";
+    const level = 1;
+    const size = "large";
+    const margin = "mb-8";
+    const tracking = "tracking-normal";
+
+    render(
+      <Heading
+        a11yTitle={a11yTitle}
+        level={level}
+        size={size}
+        margin={margin}
+        tracking={tracking}
+      >
+        Heading 1
+      </Heading>
+    );
 
     expect(screen.getByRole("heading").textContent).toBe("Heading 1");
+    expect(screen.getByRole("heading").getAttribute("aria-label")).toBe(
+      "a11y Title"
+    );
+    expect(screen.getByRole("heading").classList).toContain(size);
+    expect(screen.getByRole("heading").classList).toContain(margin);
+    expect(screen.getByRole("heading").classList).toContain(tracking);
+  });
+
+  it("should render a heading with default props", () => {
+    const text = "This is a heading";
+
+    const defaults = {
+      size: "xlarge",
+      weight: "font-extrabold",
+      margin: "mb-4",
+      tracking: "tracking-tight",
+    };
+
+    render(<Heading>{text}</Heading>);
+    expect(screen.getByRole("heading").textContent).toBe(text);
+    expect(screen.getByRole("heading").getAttribute("aria-label")).toBe(null);
+    expect(screen.getByRole("heading").classList).toContain(defaults.size);
+    expect(screen.getByRole("heading").classList).toContain(defaults.weight);
+    expect(screen.getByRole("heading").classList).toContain(defaults.margin);
+    expect(screen.getByRole("heading").classList).toContain(defaults.tracking);
   });
 });
