b4c2002e0272f78809d6d75911c50795b14429b3
Florian Rand <florian.rand@gmail.com>
2021-11-08 11:50:33 +0100

    Add example with image

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,11 @@ export const Index = (): JSX.Element => (
         valueProposition="Data to enrich your online business"
         valueDescription="Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua."
       />
+      <Hero
+        valueProposition="Data to enrich your online business"
+        valueDescription="Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua."
+        image={<Image src={placeholder} alt="placeholder" />}
+      />
       <Feature
         image={
           <Image
