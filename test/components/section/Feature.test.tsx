2c714079abf824dd39fba59bc60c842688e5092f
Florian Rand <florian.rand@gmail.com>
2021-11-08 11:44:23 +0100

    Add feature component test

---
+++ test/components/section/Feature.test.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { render, screen } from "@testing-library/react";
+import Feature from "../../../components/section/Feature";
+import placeholder from "../../../public/1920x1280.png";
+
+describe("Feature", () => {
+  it("Should render a image with content", () => {
+    render(
+      <Feature
+        image={
+          <Image
+            src={placeholder}
+            height={1280}
+            width={1920}
+            alt="placeholder image"
+            placeholder="empty"
+          />
+        }
+      >
+        <div>Hello</div>
+      </Feature>
+    );
+    const image = screen.getByRole("img");
+    expect(image).toBeInTheDocument();
+    expect(screen.getAllByText("Hello")).toBeInTheDocument;
+  });
+});
