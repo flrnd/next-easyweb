8755fca4edeaef4ac0b7ae90e1c08d915c3f49f5
Florian Rand <florian.rand@gmail.com>
2021-11-08 12:16:04 +0100

    Refactor Hero to accept CTA

--- components/section/Hero.tsx
+++ components/section/Hero.tsx
@@ -1,38 +1,24 @@
 import { ReactNode } from "react";
-import Button from "../elements/Button";
 import Heading from "../elements/Heading";
 
 interface Props {
   valueProposition: string;
   valueDescription: string;
+  cta?: ReactNode;
   image?: ReactNode;
 }
 
 const Hero = ({
   valueProposition,
   valueDescription,
+  cta,
   image,
 }: Props): JSX.Element => {
   return (
     <div className="hero">
       <Heading>{valueProposition}</Heading>
       <p>{valueDescription}</p>
-      <div className="mt-8 md:mt-5 flex justify-center">
-        <Button
-          bgColor="bg-indigo-600"
-          textColor="text-white"
-          hoverBg="bg-indigo-700"
-          path="/"
-          text="Get Started"
-        />
-        <Button
-          bgColor="bg-indigo-100"
-          textColor="text-indigo-700"
-          hoverBg="bg-indigo-200"
-          path="#"
-          text="Live Demo"
-        />
-      </div>
+      {cta && <div className="mt-8 md:mt-5 flex justify-center">{cta}</div>}
       {image && <div className="mt-5 md:mt-8">{image}</div>}
     </div>
   );
