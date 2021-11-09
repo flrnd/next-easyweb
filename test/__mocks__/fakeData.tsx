aec635057e3b6e26b07c217b03325a4aa73ad2eb
Florian Rand <florian.rand@gmail.com>
2021-11-09 13:00:13 +0100

    Refactor fakedata with icon sizes

--- test/__mocks__/fakeData.tsx
+++ test/__mocks__/fakeData.tsx
@@ -10,28 +10,28 @@ const services = [
     title: "Web Development",
     description:
       "We build websites that are fast, secure, and easy to use. We use modern technologies to make your website look great and work flawlessly.",
-    icon: <CodeIcon className="mx-auto mb-4 icon-small" />,
+    icon: <CodeIcon className="service-icon" />,
     href: "/web-development",
   },
   {
     title: "Mobile Development",
     description:
       "We build mobile apps that are fast, secure, and easy to use. We use modern technologies to make your app look great and work flawlessly.",
-    icon: <DeviceMobileIcon className="mx-auto mb-4 icon-small" />,
+    icon: <DeviceMobileIcon className="service-icon" />,
     href: "/mobile-development",
   },
   {
     title: "API Development",
     description:
       "We build APIs that are fast, secure, and easy to use. We use modern technologies to make your API look great and work flawlessly.",
-    icon: <DatabaseIcon className="mx-auto mb-4 icon-small" />,
+    icon: <DatabaseIcon className="service-icon" />,
     href: "/api-development",
   },
   {
     title: "Cloud Development",
     description:
       "We build cloud solutions that are fast, secure, and easy to use. We use modern technologies to make your solution look great and work flawlessly.",
-    icon: <CloudIcon className="mx-auto mb-4 icon-small" />,
+    icon: <CloudIcon className="service-icon" />,
     href: "/cloud-development",
   },
 ];
