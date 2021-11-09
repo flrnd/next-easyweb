62339d4483a7f723b450cd37f8716235582adecb
Florian Rand <florian.rand@gmail.com>
2021-11-09 15:11:17 +0100

    Add service component test

---
+++ test/components/content/Service.test.tsx
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react";
+import { Service } from "../../../components/content";
+import { serviceItem } from "../../__mocks__/fakeData";
+
+describe("Service", () => {
+  it("renders an icon, heading, description and a link", () => {
+    const { getByRole, getByText } = render(
+      <Service
+        icon={serviceItem.icon}
+        name={serviceItem.name}
+        text={serviceItem.description}
+        href={serviceItem.href}
+      />
+    );
+
+    const icon = document.querySelector("svg");
+    expect(icon).toBeInTheDocument();
+    expect(getByRole("heading")).toBeInTheDocument();
+    expect(getByText(serviceItem.description)).toBeInTheDocument();
+    expect(getByRole("link")).toBeInTheDocument();
+  });
+});
