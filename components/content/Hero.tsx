6c44f3c2eb09334efaabd7079c785f6b596bc41d
Florian Rand <florian.rand@gmail.com>
2021-11-09 16:42:30 +0100

    Refactor css and styling

--- components/content/Hero.tsx
+++ components/content/Hero.tsx
@@ -11,9 +11,9 @@ interface Props {
 const Hero = ({ heading, text, cta, image }: Props): JSX.Element => {
   return (
     <div className="hero">
-      <Heading>{heading}</Heading>
+      <Heading margin="mb-6 md:mb-8">{heading}</Heading>
       <p>{text}</p>
-      {cta && <div className="mt-8 md:mt-5 flex justify-center">{cta}</div>}
+      {cta && <div className="cta">{cta}</div>}
       {image && <div className="mt-5 md:mt-8">{image}</div>}
     </div>
   );
