c05e10f4ab9e63b984d9d7446fd94b126187c582
Florian Rand <florian.rand@gmail.com>
2021-11-09 12:52:04 +0100

    Refactor calculateGridColumns to its own file

--- components/content/ServiceList.tsx
+++ components/content/ServiceList.tsx
@@ -1,23 +1,15 @@
 import { Service } from ".";
 import { IServiceListItem } from "../../types/interfaces";
+import calculateGridColumns from "../../util/calculateGridColumns";
 
 interface IProps {
   services: IServiceListItem[];
 }
 
-const calculateGridColumns = (length: number) => {
-  switch (length % 2 === 0) {
-    case true:
-      return 2;
-    case false:
-      return 3;
-  }
-};
-
 const ServiceList = ({ services }: IProps): JSX.Element => {
   return (
     <div
-      className={`grid grid-cols-${calculateGridColumns(
+      className={`my-4 grid grid-cols-${calculateGridColumns(
         services.length
       )} gap-4`}
     >
