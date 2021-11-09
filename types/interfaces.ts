4fc7da944bec4bfe3e66da7e79f901d3912426c1
Florian Rand <florian.rand@gmail.com>
2021-11-09 12:38:54 +0100

    Add IServiceListItem interface

--- types/interfaces.ts
+++ types/interfaces.ts
@@ -2,3 +2,10 @@ export interface INavigationListItem {
   name: string;
   href: string;
 }
+
+export interface IServiceListItem {
+  title: string;
+  description: string;
+  icon: JSX.Element;
+  href?: string;
+}
