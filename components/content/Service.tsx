d04fdbd0f9c0f9f3cce8ed2c1f6f8060bac91a1e
Florian Rand <florian.rand@gmail.com>
2021-11-10 10:07:39 +0100

    Add paragraph and adjust sizeç

--- components/content/Service.tsx
+++ components/content/Service.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { ReactNode } from "react";
 import stringToId from "../../util/stringToId";
 import Heading from "../typography/Heading";
+import Paragraph from "../typography/Paragraph";
 interface IProps {
   icon: ReactNode;
   name: string;
@@ -21,12 +22,14 @@ const Service = ({
     <>
       <div id={stringToId(name)} className="service">
         {icon && <div className="icon icon-medium">{icon}</div>}
-        <Heading level={5} size="xsmall" weight="font-bold">
+        <Heading level={5} size="small" weight="font-bold">
           {name}
         </Heading>
-        <p className="text-lg mb-8">{text}</p>
+        <Paragraph margin="mb-6 md:mb-8" size="small">
+          {text}
+        </Paragraph>
         <Link href={href}>
-          <a className="mt-5 font-semibold mb-4 underline">{linkText}</a>
+          <a className="small mt-5 font-semibold mb-4 underline">{linkText}</a>
         </Link>
       </div>
     </>
