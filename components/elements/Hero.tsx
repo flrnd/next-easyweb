76e29eae4fa9cae7c1e459b350acce6525f2474c
Florian Rand <florian.rand@gmail.com>
2021-11-10 11:24:20 +0100

    Refactor with container

--- components/elements/Hero.tsx
+++ components/elements/Hero.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { Container } from "../layout/";
 import { Heading, Paragraph } from "../typography";
 
 interface Props {
@@ -10,14 +11,16 @@ interface Props {
 
 const Hero = ({ heading, text, cta, image }: Props): JSX.Element => {
   return (
-    <div className="hero">
-      <Heading margin="mb-6 md:mb-8">{heading}</Heading>
-      <Paragraph margin="mb-6 md:mb-8" size="medium">
-        {text}
-      </Paragraph>
-      {cta && <div className="cta">{cta}</div>}
-      {image && <div className="mt-5 md:mt-8">{image}</div>}
-    </div>
+    <Container>
+      <div className="hero">
+        <Heading margin="mb-6 md:mb-8">{heading}</Heading>
+        <Paragraph margin="mb-6 md:mb-8" size="medium">
+          {text}
+        </Paragraph>
+        {cta && <div className="cta">{cta}</div>}
+        {image && <div className="mt-5 md:mt-8">{image}</div>}
+      </div>
+    </Container>
   );
 };
 
