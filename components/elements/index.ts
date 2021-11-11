314e08b2a3c4326bfe52b06080eea9d4d34015bf
Florian Rand <florian.rand@gmail.com>
2021-11-11 14:36:47 +0100

    Update export

--- components/elements/index.ts
+++ components/elements/index.ts
@@ -2,6 +2,18 @@ import Card from "./Card";
 import Hero from "./Hero";
 import Service from "./Service";
 import ServiceList from "./ServiceList";
+import IconLink from "./IconLink";
+import IconLinkList from "./IconLinkList";
 import SplitSide from "./SplitSide";
+import Member from "./Member";
 
-export { Card, SplitSide, Hero, Service, ServiceList };
+export {
+  Card,
+  Member,
+  IconLink,
+  IconLinkList,
+  SplitSide,
+  Hero,
+  Service,
+  ServiceList,
+};
