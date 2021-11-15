f7bc9e97423cfda8426c8f439311b949b4a81cd1
Florian Rand <florian.rand@gmail.com>
2021-11-15 11:14:04 +0100

    Placeholder contact us page

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -1,12 +1,19 @@
 import Contact from "../components/elements/Contact";
+import { Container } from "../components/layout";
 import { contactData } from "../test/__mocks__/fakeData";
 
 const ContactUs = (): JSX.Element => {
   return (
     <>
       <main>
-        <h1>Contact Us</h1>
-        <Contact contact={contactData} />
+        <Container size="max-w-lg" margin="mx-5">
+          <Contact
+            title="Contacto"
+            description="Para cualquier duda no dude en contactar con nosotros"
+            size="medium"
+            contactData={contactData}
+          />
+        </Container>
       </main>
     </>
   );
