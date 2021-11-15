dd15cae72b9bfa0a91071e8b58041a3690778963
Florian Rand <florian.rand@gmail.com>
2021-11-15 11:13:49 +0100

    Remove import

--- components/elements/Contact.tsx
+++ components/elements/Contact.tsx
@@ -1,17 +1,54 @@
+import {
+  ClockIcon,
+  LocationMarkerIcon,
+  MailIcon,
+  PhoneIcon,
+} from "@heroicons/react/outline";
+import classNames from "classnames";
 import { IContact } from "../../types/interfaces";
+import { Heading, Paragraph } from "../typography";
+import ContactItem from "./ContactItem";
 
 interface IProps {
-  contact: IContact;
+  title: string;
+  description: string;
+  size?: string;
+  contactData: IContact;
 }
 
-const Contact = ({ contact }: IProps): JSX.Element => {
+const Contact = ({
+  title,
+  description,
+  size,
+  contactData,
+}: IProps): JSX.Element => {
   return (
-    <div>
-      <div>{contact.address}</div>
-      <div>{contact.hours}</div>
-      <div>{contact.phone}</div>
-      <div>{contact.email}</div>
-    </div>
+    <>
+      <Heading level={2}>{title}</Heading>
+      <Paragraph size={size}>{description}</Paragraph>
+      <ul className={classNames("p-2", size)}>
+        <ContactItem
+          title="Horarios"
+          icon={<ClockIcon />}
+          value={contactData.hours}
+        />
+        <ContactItem
+          title="Dirección"
+          icon={<LocationMarkerIcon />}
+          value={contactData.address}
+        />
+        <ContactItem
+          title="Teléfono"
+          icon={<PhoneIcon />}
+          value={contactData.phone}
+        />
+        <ContactItem
+          title="Correo"
+          icon={<MailIcon />}
+          value={contactData.email}
+        />
+      </ul>
+    </>
   );
 };
 
