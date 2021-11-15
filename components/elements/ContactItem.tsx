5538c375c75a5d279f87ca7706631857d697e398
Florian Rand <florian.rand@gmail.com>
2021-11-15 12:48:24 +0100

    Refactor contactItem, remove function

--- components/elements/ContactItem.tsx
+++ components/elements/ContactItem.tsx
@@ -1,4 +1,3 @@
-import { ReactNode } from "react";
 import { getIcon } from "../icons";
 
 interface IProps {
@@ -11,22 +10,16 @@ const ContactItem = ({ title, icon, value }: IProps): JSX.Element => {
   return (
     <li className="mb-4 md:mb-6">
       <div className="flex items-center mb-2">
-        <div className="text-gray-400 icon-small mr-2">{getIcon(icon)}</div>
+        <div className="icon-small mr-2">{getIcon(icon)}</div>
         <div className="font-bold">{title}</div>
       </div>
-      <div className="ml-10">{parseValue(value)}</div>
+      <div className="ml-10">
+        {value.map((v) => (
+          <div key={v}>{v}</div>
+        ))}
+      </div>
     </li>
   );
 };
-
-function parseValue(value: string[]): ReactNode {
-  const valueNode = value.map((v) => (
-    <div key={v} className="text-gray-600">
-      {v}
-    </div>
-  ));
-
-  return valueNode;
-}
 
 export default ContactItem;
