6c0ce20bc3efd7bac5c24e241a75f7bc3d624fdd
Florian Rand <florian.rand@gmail.com>
2021-11-15 12:33:50 +0100

    Update refactor getIconQ

--- components/elements/SocialIconList.tsx
+++ components/elements/SocialIconList.tsx
@@ -1,5 +1,5 @@
 import { ISocial } from "../../types/interfaces";
-import { getSocialIcon } from "../icons";
+import { getIcon } from "../icons";
 import IconLink from "./IconLink";
 
 interface IProps {
@@ -10,11 +10,7 @@ const SocialIconList = ({ items }: IProps): JSX.Element => {
   return (
     <div className="flex">
       {items.map((item) => (
-        <IconLink
-          key={item.href}
-          icon={getSocialIcon(item.icon)}
-          href={item.href}
-        />
+        <IconLink key={item.href} icon={getIcon(item.icon)} href={item.href} />
       ))}
     </div>
   );
