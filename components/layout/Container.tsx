7f42cb6fa06fd0b8d2980b7f3f691a2e554388d3
Florian Rand <florian.rand@gmail.com>
2021-11-15 11:14:52 +0100

    Refactor container component to accept size and margin

--- components/layout/Container.tsx
+++ components/layout/Container.tsx
@@ -1,10 +1,19 @@
+import classNames from "classnames";
 import { ReactNode } from "react";
 
 interface IProps {
+  size?: string;
+  margin?: string;
   children: ReactNode;
 }
-const Container = ({ children }: IProps): JSX.Element => {
-  return <div className="container mx-auto">{children}</div>;
+const Container = ({
+  size,
+  margin = "mx-auto",
+  children,
+}: IProps): JSX.Element => {
+  return (
+    <div className={classNames("container", size, margin)}>{children}</div>
+  );
 };
 
 export default Container;
