16994afb22fc1bcb8b887ff1699e6737c5f40986
Florian Rand <florian.rand@gmail.com>
2021-11-16 12:53:21 +0100

    Refactor footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -8,38 +8,40 @@ const contact = ["Email: hello@example.com", "Phone: 123-456-789"];
 const Footer = (): JSX.Element => {
   return (
     <footer className="footer">
-      <div className="flex flex-col md:flex-row md:justify-between py-12">
-        <div id="logo" className="footer-section logo">
-          <Logo
-            src="/assets/pictures/brand/vitary-logo.png"
-            width={237}
-            height={64}
-          />
-        </div>
-        <div className="flex flex-col md:flex-row md:w-1/2 md:justify-end">
-          <div id="nav" className="footer-section">
-            <div className="text-2xl mb-2">Enlaces</div>
-            <NavigationList isVertical={true} list={list} />
+      <div className="container mx-auto">
+        <div className="flex flex-col md:flex-row md:justify-between py-12">
+          <div id="logo" className="footer-section logo">
+            <Logo
+              src="/assets/pictures/brand/vitary-logo.png"
+              width={237}
+              height={64}
+            />
           </div>
-          <div className="footer-section">
-            <div className="text-2xl mb-2">Horario</div>
-            <div>
-              {hours.map((h) => (
-                <div key={h}>{h}</div>
-              ))}
+          <div className="flex flex-col md:flex-row md:w-1/2 md:justify-end">
+            <div id="nav" className="footer-section">
+              <div className="text-2xl mb-2">Enlaces</div>
+              <NavigationList isVertical={true} list={list} />
             </div>
-          </div>
-          <div className="footer-section">
-            <div className="text-2xl mb-2">Contacto</div>
-            <div>
-              {contact.map((m) => (
-                <div key={m}>{m}</div>
-              ))}
+            <div className="footer-section">
+              <div className="text-2xl mb-2">Horario</div>
+              <div>
+                {hours.map((h) => (
+                  <div key={h}>{h}</div>
+                ))}
+              </div>
+            </div>
+            <div className="footer-section">
+              <div className="text-2xl mb-2">Contacto</div>
+              <div>
+                {contact.map((m) => (
+                  <div key={m}>{m}</div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className=" bg-red-800 text-center text-white py-2">
+      <div className="bg-red-800 text-center text-white py-2">
         copyright(c) 2022 marca privacidad
       </div>
     </footer>
