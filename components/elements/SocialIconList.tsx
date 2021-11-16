7fbac41d2b31a39c7a4cac97fe710f0cc9ec26a0
Florian Rand <florian.rand@gmail.com>
2021-11-16 15:09:10 +0100

    Refactor social icon lists with justify, margin

--- components/elements/SocialIconList.tsx
+++ components/elements/SocialIconList.tsx
@@ -1,16 +1,39 @@
+import classNames from "classnames";
 import { ISocial } from "../../types/interfaces";
 import { getIcon } from "../icons";
 import IconLink from "./IconLink";
 
 interface IProps {
   items: ISocial[];
+  margin?: string;
+  justify?: "center" | "left" | "right" | "between";
 }
 
-const SocialIconList = ({ items }: IProps): JSX.Element => {
+const getJustify = (justify: IProps["justify"]) => {
+  switch (justify) {
+    case "center":
+      return "justify-center";
+    case "left":
+      return "justify-start";
+    case "right":
+      return "justify-end";
+    case "between":
+      return "justify-between";
+    default:
+      return "justify-center";
+  }
+};
+
+const SocialIconList = ({ items, margin, justify }: IProps): JSX.Element => {
   return (
-    <div className="flex">
+    <div className={classNames("flex", "w-full", getJustify(justify))}>
       {items.map((item) => (
-        <IconLink key={item.href} icon={getIcon(item.icon)} href={item.href} />
+        <IconLink
+          key={item.href}
+          icon={getIcon(item.icon)}
+          href={item.href}
+          margin={margin}
+        />
       ))}
     </div>
   );
