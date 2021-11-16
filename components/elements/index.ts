b5bf45e47105b222528e9f93cc42044c4fed0549
Florian Rand <florian.rand@gmail.com>
2021-11-16 12:36:57 +0100

    Add elements to index export

--- components/elements/index.ts
+++ components/elements/index.ts
@@ -1,4 +1,6 @@
 import Card from "./Card";
+import Contact from "./Contact";
+import ContactItem from "./ContactItem";
 import Hero from "./Hero";
 import Service from "./Service";
 import ServiceList from "./ServiceList";
@@ -6,14 +8,18 @@ import IconLink from "./IconLink";
 import SocialIconList from "./SocialIconList";
 import SplitSide from "./SplitSide";
 import Member from "./Member";
+import Logo from "./Logo";
 
 export {
   Card,
+  Contact,
+  ContactItem,
   Member,
   IconLink,
   SocialIconList,
   SplitSide,
   Hero,
+  Logo,
   Service,
   ServiceList,
 };
