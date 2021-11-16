c7eef72ffb9abd8b0e8a804b363b8099abaa7523
Florian Rand <florian.rand@gmail.com>
2021-11-16 13:25:27 +0100

    Adjust contact us image

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -1,11 +1,12 @@
+import Image from "next/image";
 import Contact from "../components/elements/Contact";
 import { Container } from "../components/layout";
-import { contactDataList } from "../test/__mocks__/fakeData";
+import { contactDataList, wideImage } from "../test/__mocks__/fakeData";
 
 const ContactUs = (): JSX.Element => {
   return (
     <>
-      <main>
+      <main className="container mx-auto flex flex-col-reverse md:flex-row">
         <Container size="max-w-lg" margin="mx-5">
           <Contact
             title="Contacto"
@@ -14,6 +15,9 @@ const ContactUs = (): JSX.Element => {
             list={contactDataList}
           />
         </Container>
+        <div className="relative h-64 w-full mb-4 md:mb-0 md:h-auto">
+          <Image src={wideImage} layout="fill" objectFit="cover" />
+        </div>
       </main>
     </>
   );
