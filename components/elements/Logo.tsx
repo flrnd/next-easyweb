e8a4479deebd8c26fae84412a72fd8fc002e75ad
Florian Rand <florian.rand@gmail.com>
2021-11-17 11:30:47 +0100

    Refactor logo component to be responsive

--- components/elements/Logo.tsx
+++ components/elements/Logo.tsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
 interface IProps {
-  src: string;
+  src: string | StaticImageData;
   width: number | string;
   height: number | string;
 }
 
 const Logo = ({ src, width, height }: IProps): JSX.Element => {
   return (
-    <div className="relative" style={{ width: width, height: height }}>
-      <Image layout="fill" objectFit="contain" src={src} alt="logo" />
+    <div className="w-40 md:w-48 lg:w-56">
+      <Image
+        layout="responsive"
+        objectFit="contain"
+        src={src}
+        alt="logo"
+        width={width}
+        height={height}
+      />
     </div>
   );
 };
