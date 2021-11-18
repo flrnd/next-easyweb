8ab4061502458d8c5673a90cd06ba02852023950
Florian Rand <florian.rand@gmail.com>
2021-11-18 11:25:07 +0100

    Add Logo test case

---
+++ test/components/elements/Logo.test.tsx
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react";
+import { Logo } from "../../../components/elements";
+import { logotype } from "../../__mocks__/fakeData.tsx";
+
+describe("Logo", () => {
+  it("renders a logotype", () => {
+    render(
+      <Logo
+        src={logotype.src}
+        width={logotype.dimensions.width}
+        height={logotype.dimensions.height}
+      />
+    );
+
+    expect(document.querySelector("img")).toBeInTheDocument();
+  });
+});
