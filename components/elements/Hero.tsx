da8d155c0a04799b8b6fb5296a33c5c4b19d2ccd
Florian Rand <florian.rand@gmail.com>
2021-11-19 14:40:29 +0100

    Refactor Hero layout. Full w sized images

--- components/elements/Hero.tsx
+++ components/elements/Hero.tsx
@@ -11,16 +11,16 @@ interface Props {
 
 const Hero = ({ heading, text, cta, image }: Props): JSX.Element => {
   return (
-    <Container>
-      <div className="hero">
+    <div className="hero">
+      <Container>
         <Heading margin="mb-6 md:mb-8">{heading}</Heading>
         <Paragraph margin="mb-6 md:mb-8" size="medium">
           {text}
         </Paragraph>
         {cta && <div className="cta">{cta}</div>}
-        {image && <div className="mt-5 md:mt-8">{image}</div>}
-      </div>
-    </Container>
+      </Container>
+      {image && <div className="mt-5 md:mt-8">{image}</div>}
+    </div>
   );
 };
 
