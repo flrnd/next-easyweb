4a63d0cf8ec6045589b0ae2d2a85d888a12ec372
Florian Rand <florian.rand@gmail.com>
2021-11-22 11:54:48 +0100

    Add burger menu test case

---
+++ test/components/navigation/BurgerMenu.test.tsx
@@ -0,0 +1,21 @@
+import { cleanup, render } from "@testing-library/react";
+import { BurgerMenu } from "../../../components/navigation";
+
+afterAll(cleanup);
+
+describe("Burger Menu", () => {
+  const menu = ["services", "about", "contact us"];
+
+  it("renders mobile and desktop menus", () => {
+    const { getAllByRole } = render(<BurgerMenu menu={menu} />);
+
+    expect(getAllByRole("listitem")).toHaveLength(menu.length * 2);
+  });
+
+  it("render a button", () => {
+    const { getByTitle } = render(<BurgerMenu menu={menu} />);
+
+    const menuButton = getByTitle("menu");
+    expect(menuButton).toBeInTheDocument();
+  });
+});
