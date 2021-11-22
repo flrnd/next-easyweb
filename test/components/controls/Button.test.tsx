c39b858c099ac5571081d1d2b5bba9d3e7cd4fd9
Florian Rand <florian.rand@gmail.com>
2021-11-22 12:43:52 +0100

    Refactor button test

--- test/components/controls/Button.test.tsx
+++ test/components/controls/Button.test.tsx
@@ -1,25 +1,50 @@
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { Button } from "../../../components/controls";
 
 describe("Button", () => {
-  it("should render a button with link and css classes", () => {
+  it("should render a button with link and props", () => {
     const baseURL = "http://localhost";
     const text = "Get Started";
     const path = "/get-started";
-    render(
+    const { getByRole } = render(
       <Button
         label={text}
         background="bg-red-200"
         textColor="text-white"
+        rounded="rounded-md"
+        shadow="shadow-md"
+        margin="mr-4"
+        hoverBg="bg-red-300"
+        hover="text-red-500"
         href={path}
       />
     );
 
-    const link = screen.getByRole("link") as HTMLAnchorElement;
+    const link = getByRole("link") as HTMLAnchorElement;
+    const button = getByRole("button");
 
     expect(link.href).toBe(`${baseURL}${path}`);
-    expect(screen.getByRole("link").textContent).toBe(text);
-    expect(screen.getByRole("button").classList).toContain("bg-red-200");
-    expect(screen.getByRole("button").classList).toContain("text-white");
+    expect(link.textContent).toBe(text);
+
+    [
+      "bg-red-200",
+      "text-white",
+      "rounded-md",
+      "shadow-md",
+      "mr-4",
+      "hover:bg-red-300",
+      "hover:text-red-500",
+    ].forEach((item) => {
+      expect(button.classList).toContain(item);
+    });
+  });
+
+  it("Calls 'onClick' prop on click", () => {
+    const handleClick = jest.fn();
+
+    const { getByRole } = render(<Button onClick={handleClick} />);
+
+    fireEvent.click(getByRole("button"));
+    expect(handleClick).toHaveBeenCalled();
   });
 });
