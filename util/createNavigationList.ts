9ae94205483696ca348a9b6221c38cc1b5c198a6
Florian Rand <florian.rand@gmail.com>
2021-11-22 14:15:41 +0100

    Refactor to generate home | inicio menu items

--- util/createNavigationList.ts
+++ util/createNavigationList.ts
@@ -3,7 +3,14 @@ import stringToId from "./stringToId";
 
 const createNavigationList = (menu: string[]): INavigationListItem[] =>
   menu.map((item) => {
-    return { name: item, href: `/${stringToId(item)}` };
+    switch (item.toLowerCase()) {
+      case "home":
+        return { name: "Home", href: "/" };
+      case "inicio":
+        return { name: "Inicio", href: "/" };
+      default:
+        return { name: item, href: `/${stringToId(item)}` };
+    }
   });
 
 export default createNavigationList;
