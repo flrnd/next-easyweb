26c9473ad1e1929a55bd156ed87bc740dcf6ebeb
Florian Rand <florian.rand@gmail.com>
2021-11-23 12:44:16 +0100

    Add env check to scripts

--- containers/pgadmin.sh
+++ containers/pgadmin.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-podman run --pod=$PGADMIN_POD_NAME \
--e PGADMIN_DEFAULT_EMAIL=$PGADMIN_DEFAULT_EMAIL \
--e PGADMIN_DEFAULT_PASSWORD=$PGADMIN_DEFAULT_PASSWORD \
---name pgadmin12 \
--d dpage/pgadmin4 \
 
+if [ -z ${PGADMIN_POD_NAME+x} ] || [ -z ${PGADMIN_DEFAULT_EMAIL+x} ] || [ -z ${PGADMIN_DEFAULT_PASSWORD+x} ]; then
+  printf "%s\n" "export PGADMIN_PODNAME, PGADMIN_DEFAULT_EMAIL, PGADMIN_DEFAULT_PASSWORD variables";
+else
+		podman run --pod=$PGADMIN_POD_NAME \
+      -e PGADMIN_DEFAULT_EMAIL=$PGADMIN_DEFAULT_EMAIL \
+      -e PGADMIN_DEFAULT_PASSWORD=$PGADMIN_DEFAULT_PASSWORD \
+      --name pgadmin12 \
+      -d dpage/pgadmin4;
+fi
--- containers/postgreserver.sh
+++ containers/postgreserver.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-podman run --pod=$POSTGRES_DB_POD_NAME \
--v $HOME/.local/share/containers/volume:/var/lib/postgresql/data:Z \
--e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
--e POSTGRES_USER=$POSTGRES_USER \
---name database -d postgres \
+
+if [ -z ${POSTGRES_DB_POD_NAME+x} ] || [ -z ${POSTGRES_PASSWORD+x} ] || [ -z ${POSTGRES_USER+x} ]; then
+  printf "%s\n" "export POSTGRES_DB_POD_NAME, POSTGRES_USER, POSTGRES_PASSWORD";
+else
+  podman run --pod=$POSTGRES_DB_POD_NAME \
+    -v $HOME/.local/share/containers/volume:/var/lib/postgresql/data:Z \
+    -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+    -e POSTGRES_USER=$POSTGRES_USER \
+    --name database \
+    -d postgres;
+fi
 
