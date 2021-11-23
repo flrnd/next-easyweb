c8e5a4e62dd141ccb32653f7aeccf94021869161
Florian Rand <florian.rand@gmail.com>
2021-11-23 12:23:28 +0100

    Env variables

--- containers/postgreserver.sh
+++ containers/postgreserver.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-podman run --pod=postgre-server \
--v ~/.local/share/containers/volume:/var/lib/postgresql/data:Z \
--e POSTGRES_PASSWORD=Cahkao7i \
--e POSTGRES_USER=dbadm \
+podman run --pod=$POSTGRES_DB_POD_NAME \
+-v $HOME/.local/share/containers/volume:/var/lib/postgresql/data:Z \
+-e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+-e POSTGRES_USER=$POSTGRES_USER \
 --name database -d postgres \
 
