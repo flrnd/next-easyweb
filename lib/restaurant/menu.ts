584342997365223b2c44f1ca57a23b1a58bbf5bd
Florian Rand <florian.rand@gmail.com>
2021-11-23 11:00:46 +0100

    Add restaurant data

---
+++ lib/restaurant/menu.ts
@@ -0,0 +1,34 @@
+const menu = [
+  {
+    name: "Pizza",
+    price: 10,
+    ingredients: ["tomato", "cheese", "sauce"],
+    image: "https://source.unsplash.com/random",
+  },
+  {
+    name: "Spaghetti",
+    price: 15,
+    ingredients: ["tomato", "cheese", "sauce"],
+    image: "https://source.unsplash.com/random",
+  },
+  {
+    name: "Soup",
+    price: 5,
+    ingredients: ["tomato", "cheese", "sauce"],
+    image: "https://source.unsplash.com/random",
+  },
+  {
+    name: "Salad",
+    price: 10,
+    ingredients: ["tomato", "cheese", "lettuce"],
+    image: "https://source.unsplash.com/random",
+  },
+  {
+    name: "Cake",
+    price: 20,
+    ingredients: ["tomato", "cheese", "sauce"],
+    image: "https://source.unsplash.com/random",
+  },
+];
+
+export default menu;
---
+++ lib/restaurant/restaurant.ts
@@ -0,0 +1,76 @@
+import logotype from "../../public/assets/gondoliere_logo.svg";
+import menu from "./menu";
+import services from "./services";
+import testimonials from "./testimonials";
+
+const restaurant = {
+  name: "Restaurant",
+  description: "Restaurant",
+  keywords: "Restaurant, Italian, food, premium",
+  logo: logotype,
+  contact: {
+    phone: ["123-456-7890", "098-765-4321"],
+    email: ["hello@example.com"],
+    address: ["123 Main St, Anytown, CA 12345"],
+    hours: ["Mon-Fri: 9am-5pm", "Sat: 10am-2pm"],
+  },
+  header: {
+    navigation: [],
+    cta: {
+      text: "Order Now",
+      url: "/order",
+    },
+  },
+  footer: {
+    social: [],
+    openingHours: [],
+    contact: [],
+    legal: [],
+    siteMap: [],
+  },
+  pages: {
+    home: {
+      title: "Restaurant",
+      description: "Restaurant",
+      keywords: "Restaurant, Italian, food, premium",
+      hero: {
+        title: "Restaurant",
+        subtitle: "Restaurant",
+        image: "https://source.unsplash.com/random",
+      },
+      introSection: {
+        title: "Restaurant",
+        subtitle: "Restaurant",
+        image: "https://source.unsplash.com/random",
+        button: {
+          text: "Learn more",
+          link: "/about-us",
+        },
+      },
+      services,
+      menu: {
+        items: menu,
+        button: {
+          text: "View menu",
+          link: "/menu",
+        },
+      },
+      bookingSection: {
+        title: "Restaurant",
+        subtitle: "Restaurant",
+        image: "https://source.unsplash.com/random",
+        button: {
+          text: "Book a table",
+          link: "/book-a-table",
+        },
+      },
+      testimonials: {
+        title: "Restaurant",
+        subtitle: "Restaurant",
+        items: testimonials,
+      },
+    },
+  },
+};
+
+export default restaurant;
