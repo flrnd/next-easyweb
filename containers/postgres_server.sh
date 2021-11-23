5c413ead96530dad452f39b5e39046e72e74e624
Florian Rand <florian.rand@gmail.com>
2021-11-23 16:11:36 +0100

    Postgresql script

--- containers/postgres_server.sh
+++ containers/postgres_server.sh
@@ -1,13 +1,53 @@
 #!/bin/sh
 
-if [ -z ${POSTGRES_DB_POD_NAME+x} ] || [ -z ${POSTGRES_PASSWORD+x} ] || [ -z ${POSTGRES_USER+x} ]; then
-  printf "%s\n" "export POSTGRES_DB_POD_NAME, POSTGRES_USER, POSTGRES_PASSWORD";
+VERSION=0
+REVISION=2
+
+print_help() {
+   # Display Help
+   echo "$0 ceate a postgresql container inside a pod."
+   echo
+   echo "Syntax: $0 [-n|u|p|d|c|h|v]"
+   echo "options:"
+   echo "n     set pod name."
+   echo "u     set postgresql user."
+   echo "p     set postgresql password."
+   echo "d     set postgresql image name."
+   echo "c     set container name"
+   echo "v     set container volume path."
+   echo "h     Print this Help."
+   echo "V     Print version and exit."
+   echo
+   echo "Example: $0 -n podname -u pguser -p pgpwd -d postgresql -c database -v ~/.local/share/containers/volume"
+   echo
+}
+
+while getopts "n:u:p:d:c:v:hV" flag; do
+  case $flag in
+    n) POD_NAME=${OPTARG};;
+    u) USER=${OPTARG};;
+    p) PASSWORD=${OPTARG};;
+    d) IMAGE_NAME=${OPTARG};;
+    c) CONTAINER_NAME=${OPTARG};;
+    v) VOLUME=${OPTARG};;
+    h) print_help
+      exit;;
+    V) echo "$0 $VERSION.$REVISION"
+      exit;;
+    *) print_help
+      exit 1;;
+  esac
+done
+
+if [ -z ${POD_NAME+x} ] || [ -z ${USER+x} ] || [ -z ${PASSWORD+x} ] || [ -z ${CONTAINER_NAME+x} ] || [ -z ${VOLUME+x} ]; then
+  echo "$POD_NAME $USER $PASSWORD $CONTAINER_NAME $IMAGE_NAME $VOLUME"
+  print_help
 else
-  podman run --pod=$POSTGRES_DB_POD_NAME \
-    -v $HOME/.local/share/containers/volume:/var/lib/postgresql/data:Z \
-    -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
-    -e POSTGRES_USER=$POSTGRES_USER \
-    --name database \
-    -d postgres;
+  podman run --pod=$POD_NAME \
+    -v $VOLUME:/var/lib/postgresql/data:Z \
+    -e POSTGRES_PASSWORD=$PASSWORD \
+    -e POSTGRES_USER=$USER \
+    --name $CONTAINER_NAME \
+    -d $IMAGE_NAME;
 fi
 
