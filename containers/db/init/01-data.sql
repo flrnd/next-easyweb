81dff44ef08e16f82661f99cb77c3664d3233f19
Florian Rand <florian.rand@gmail.com>
2021-11-24 13:17:36 +0100

    Db docker and init

---
+++ containers/db/init/01-data.sql
@@ -0,0 +1,13 @@
+\connect backend;
+
+/*Create some dummy users*/
+INSERT INTO public.user (username) VALUES
+('Benjie'),
+('Singingwolfboy'),
+('Lexius');
+
+/*Create some dummy websites*/
+INSERT INTO public.website (title, url, data, owner_id) VALUES
+('Benji', 'http://benji.com', '{"seo": "benji personal blog", "contact": {"phone": "123-456-7890", "email": "benji@email.com"} }', 1),
+('Singingwolfboy', 'http://singingwolfboy.com', '{"seo": "Singingwolfboy personal blog", "contact": {"phone": "123-456-7890", "email": "singing@email.com"}}', 2),
+('Lexius', 'http://lexius.com', '{"seo": "Lexius personal blog", "contact": {"phone": "123-456-7890", "email": "lexius@email.com"} }', 3);
