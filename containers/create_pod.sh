208acd693865bd0685cdecd0a26bd10adc46ae5f
Florian Rand <florian.rand@gmail.com>
2021-11-24 10:31:00 +0100

    Expose psql port

--- containers/create_pod.sh
+++ containers/create_pod.sh
@@ -36,5 +36,5 @@ if [ -z ${SERVER_POD_NAME+x} ] && [ -z ${PORT+x} ]; then
   print_help
 else
   printf "Creating pod... name: %s exposed port: %s\n" "$POD_NAME" "$PORT"
-  podman pod create --name $POD_NAME -p 9876:$PORT
+  podman pod create --name $POD_NAME -p 9876:$PORT -p 5432:5432
 fi
