6507557a869b4ab9f958234771df9dbc2264dd0b
Florian Rand <florian.rand@gmail.com>
2021-11-25 10:20:54 +0100

    Fix register_user function

--- containers/db/init/00-database.sql
+++ containers/db/init/00-database.sql
@@ -38,13 +38,13 @@ comment on column backend.website.data is 'The website’s data.';
 comment on column backend.website.created_at is 'The time this website was created.';
 
 create table backend_private.user_account (
-    account_id uuid PRIMARY KEY NOT NULL REFERENCES backend.user(id) on delete cascade,
+    user_id uuid PRIMARY KEY NOT NULL REFERENCES backend.user(id) on delete cascade,
     email TEXT NOT NULL UNIQUE CHECK (email ~* '^.+@.+\..+$'),
     password_hash TEXT NOT NULL
 );
 
 comment on table backend_private.user_account is 'Private information about a user’s account.';
-comment on column backend_private.user_account.account_id is 'The primary unique identifier for the account.';
+comment on column backend_private.user_account.user_id is 'The primary unique identifier for the account.';
 comment on column backend_private.user_account.email is 'The email address of the user.';
 comment on column backend_private.user_account.password_hash is 'An opaque hash of the user’s password.';
 
@@ -56,24 +56,24 @@ $$ language sql stable;
 
 comment on function backend.user_full_name(backend.user) is 'A user’s full name which is a concatenation of their first and last name.';
 
-CREATE FUNCTION backend.register_user(
-  first_name TEXT,
-  last_name TEXT,
-  email TEXT,
-  password TEXT
-) RETURNS backend.user AS $$
-DECLARE
-  user backend.user;
-BEGIN
-  INSERT INTO backend.user (first_name, last_name) VALUES
+create function backend.register_user(
+  first_name text,
+  last_name text,
+  email text,
+  password text
+) returns backend.user as $$
+declare
+  new_user backend.user;
+begin
+  insert into backend.user (first_name, last_name) values
     (first_name, last_name)
-    returning * INTO user;
+    returning * into new_user;
 
-  INSERT INTO backend_private.user_account (user_id, email, password_hash) VALUES
-    (user_id, email, crypt(password, gen_salt('bf')));
+  insert into backend_private.user_account (user_id, email, password_hash) values
+    (new_user.id, email, crypt(password, gen_salt('bf')));
 
-  RETURN user;
-END;
+  return new_user;
+end;
 $$ language plpgsql strict security definer;
 
 comment on function backend.register_user(text, text, text, text) is 'Registers a single user and creates an account in our platform.';
