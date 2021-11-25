62db90a81cc31d0cd72cc4d5a19c5fa52dd874c4
Florian Rand <florian.rand@gmail.com>
2021-11-25 15:28:48 +0100

    Move functions to a separate file

--- containers/db/init/00-database.sql
+++ containers/db/init/00-database.sql
@@ -49,71 +49,9 @@ comment on column website_private.user_account.user_id is 'The primary unique id
 comment on column website_private.user_account.email is 'The email address of the user.';
 comment on column website_private.user_account.password_hash is 'An opaque hash of the user’s password.';
 
-/* FUNCTIONS */
-
-/* function: user_full_name(user): text */
-create function website.user_full_name(u website.user) returns text as $$
-  select u.first_name || ' ' || u.last_name
-$$ language sql stable;
-
-comment on function website.user_full_name(website.user) is 'A user’s full name which is a concatenation of their first and last name.';
-
-/* function: register_user(fist_name, last_name, email, password): user */
-create function website.register_user(
-  first_name text,
-  last_name text,
-  email text,
-  password text
-) returns website.user as $$
-declare
-  new_user website.user;
-begin
-  insert into website.user (first_name, last_name) values
-    (first_name, last_name)
-    returning * into new_user;
-
-  insert into website_private.user_account (user_id, email, password_hash) values
-    (new_user.id, email, crypt(password, gen_salt('bf')));
-
-  return new_user;
-end;
-$$ language plpgsql strict security definer;
-
-comment on function website.register_user(text, text, text, text) is 'Registers a single user and creates an account in our platform.';
-
 /* type: jwt_token */
 create type website.jwt_token as (
   role text,
   user_id uuid,
   expires_at bigint
 );
-
-/* function: authenticate_user(email, password): (role, id, timestamp) */
-create function website.authenticate_user(
-  email text, password text
-  ) returns website.jwt_token as $$
-declare
-  account website_private.user_account;
-begin
-  select a.* into account
-  from website_private.user_account as a
-  where a.email = $1;
-
-  if account.password_hash = crypt(password, account.password_hash) then
-    return ('website_user', account.user_id, extract(epoch from (now() + interval '1 day')))::website.jwt_token;
-  else
-    return null;
-  end if;
-end;
-$$ language plpgsql strict security definer;
-
-comment on function website.authenticate_user(text, text) is 'Authenticates a user and returns a JWT token if the user is valid.';
-
-/* function: current_user() */
-create function website.current_user() returns website.user as $$
-  select *
-  from website.user
-  where id = nullif(current_setting('jwt.claims.user_id', true), '')::uuid
-$$ language sql stable;
-
-comment on function website.current_user() is 'Gets the user who was identified by our JWT.';
