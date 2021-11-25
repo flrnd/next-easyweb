9bb9644a707f16d93d689cb798af94813f2ef959
Florian Rand <florian.rand@gmail.com>
2021-11-25 11:12:08 +0100

    Rename schema

--- containers/db/init/01-data.sql
+++ containers/db/init/01-data.sql
@@ -11,13 +11,8 @@ WITH new_users AS (
   ('Mike', 'Donovan', 'mike@my.es', 'pdw123'),
   ('Carles', 'Puig', 'carles@del.net', '12345')) AS t (first_name, last_name, email, password)
 )
-SELECT backend.register_user(first_name, last_name, email, password) FROM new_users;
+SELECT website.register_user(first_name, last_name, email, password) FROM new_users;
 
 /*
 Create some dummy websites
-INSERT INTO public.website (title, url, data, owner_id) VALUES
-('Benji', 'http://benji.com', '{"seo": "benji personal blog", "contact": {"phone": "123-456-7890", "email": "benji@email.com"} }', 1),
-('Singingwolfboy', 'http://singingwolfboy.com', '{"seo": "Singingwolfboy personal blog", "contact": {"phone": "123-456-7890", "email": "singing@email.com"}}', 2),
-('Lexius', 'http://lexius.com', '{"seo": "Lexius personal blog", "contact": {"phone": "123-456-7890", "email": "lexius@email.com"} }', 3);
-
 */
