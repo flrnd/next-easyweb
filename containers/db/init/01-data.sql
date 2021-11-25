c6a94775233536a3e6071d22b4135a8177b9fa50
Florian Rand <florian.rand@gmail.com>
2021-11-25 10:20:42 +0100

    Comment dummy data

--- containers/db/init/01-data.sql
+++ containers/db/init/01-data.sql
@@ -1,8 +1,8 @@
 \connect backend;
 
 /*
-* Create some dummy data
-*/
+ * Create some dummy data
+
 
 INSERT INTO backend.user (first_name, last_name) VALUES
 ('John', 'Doe'), ('Jane', 'Doe'), ('Mike', 'Donovan');
@@ -17,11 +17,14 @@ accounts as (
   ('mikedot@got.com', '12345'))
   AS t(email, password)
 )
-INSERT INTO backend_private.user_account (account_id, email, password_hash)
+SELECT backend_private.user_account (user_id, email, password_hash)
 SELECT ids.id, accounts.email, accounts.password FROM ids, accounts;
 
-/*Create some dummy websites*/
+
+Create some dummy websites
 INSERT INTO public.website (title, url, data, owner_id) VALUES
 ('Benji', 'http://benji.com', '{"seo": "benji personal blog", "contact": {"phone": "123-456-7890", "email": "benji@email.com"} }', 1),
 ('Singingwolfboy', 'http://singingwolfboy.com', '{"seo": "Singingwolfboy personal blog", "contact": {"phone": "123-456-7890", "email": "singing@email.com"}}', 2),
 ('Lexius', 'http://lexius.com', '{"seo": "Lexius personal blog", "contact": {"phone": "123-456-7890", "email": "lexius@email.com"} }', 3);
+
+*/
