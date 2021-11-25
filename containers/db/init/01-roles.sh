50b8b4f343191c09ac5f20b95a2606b36772a2ec
Florian Rand <florian.rand@gmail.com>
2021-11-25 15:55:25 +0100

    Add policies etc·

--- containers/db/init/01-roles.sh
+++ containers/db/init/01-roles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-psql -U $POSTGRES_USER -d backend << EOF
+psql -U $site_configGRES_USER -d backend << EOF
 create role $W_ROLE login password '$W_ROLE_PASSWD';
 
 create role $W_ROLE_USER;
@@ -15,13 +15,37 @@ grant usage on schema website to website_anonymous, website_user;
 grant select on table website.user to website_anonymous, website_user;
 grant update, delete on table website.user to website_user;
 
-grant select on table website.post to website_anonymous, website_user;
-grant insert, update, delete on table website.post to website_user;
-grant usage on sequence website.post_id_seq to website_user;
+grant select on table website.site_config to website_anonymous, website_user;
+grant insert, update, delete on table website.site_config to website_user;
+grant usage on sequence website.site_config_id_seq to website_user;
 
 grant execute on function website.user_full_name(website.user) to website_anonymous, website_user;
 grant execute on function website.authenticate(text, text) to website_anonymous, website_user;
 grant execute on function website.current_user() to website_anonymous, website_user;
 
 grant execute on function website.register_user(text, text, text, text) to website_anonymous;
+
+alter table website.user enable row level security;
+alter table website.site_config enable row level security;
+
+create policy select_user on website.user for select
+  using (true);
+
+create policy select_site_config on website.site_config for select
+  using (true);
+
+create policy update_user on website.user for update to website_user
+  using (id = nullif(current_setting('jwt.claims.user_id', true), '')::uuid);
+
+create policy delete_user on website.user for delete to website_user
+  using (id = nullif(current_setting('jwt.claims.user_id', true), '')::uuid);
+
+create policy insert_site_config on website.site_config for insert to website_user
+  with check (owner_id = nullif(current_setting('jwt.claims.user_id', true), '')::uuid);
+
+create policy update_site_config on website.site_config for update to website_user
+  using (owner_id = nullif(current_setting('jwt.claims.user_id', true), '')::uuid);
+
+create policy delete_site_config on website.site_config for delete to website_user
+  using (owner_id = nullif(current_setting('jwt.claims.user_id', true), '')::uuid);
 EOF
