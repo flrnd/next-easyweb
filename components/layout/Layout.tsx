8d92a41da118a8115e5dc7e07953f6b4bbff131b
Florian Rand <florian.rand@gmail.com>
2021-11-29 10:45:14 +0100

    Refactor layout with header and footer

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,11 +1,21 @@
+import { DefaultSeo } from "next-seo";
 import { ReactNode } from "react";
+import { Footer, Header } from ".";
+import SEO from "../../next-seo.config";
 
 interface Props {
   children: ReactNode;
 }
 
 const Layout = ({ children }: Props): JSX.Element => {
-  return <div className="layout">{children}</div>;
+  return (
+    <div className="layout">
+      <DefaultSeo {...SEO} />
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
