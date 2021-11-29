dfdd0f31e910ce6faba132859b0345bc3c2eca73
Florian Rand <florian.rand@gmail.com>
2021-11-29 17:16:05 +0100

    auth db

--- api/login.ts
+++ api/login.ts
@@ -6,15 +6,17 @@ export default async function (
   res: VercelResponse
 ): Promise<VercelResponse> {
   const { username, password } = request.body;
-  const connectionString = process.env.DATABASE_URL;
-  const pgPool = new Pool({ connectionString });
+  const { DATABASE_URL } = process.env;
+
+  const pgPool = new Pool({ connectionString: DATABASE_URL });
 
   try {
+    await pgPool.connect();
     const queryRes = await pgPool.query(
-      `SELECT website.authenticate_user(${username}, ${password})`
+      `SELECT website.authenticate_user('${username}', '${password}')`
     );
     return res.status(200).json({
-      message: { queryRes },
+      message: JSON.stringify(queryRes),
     });
   } catch (err) {
     return res.status(500).json({ message: err.message });
