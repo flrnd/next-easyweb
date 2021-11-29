9cc88a2a42b5a18c2cb733808b6ec507fbc50a2f
Florian Rand <florian.rand@gmail.com>
2021-11-29 11:10:01 +0100

    Placeholder page WIP

--- pages/account/login.tsx
+++ pages/account/login.tsx
@@ -1,17 +1,40 @@
-import { Card } from "../../components/elements";
+import Link from "next/link";
+import { Card, Logo } from "../../components/elements";
 import { Container } from "../../components/layout";
+import { Heading } from "../../components/typography";
 
 const Login = (): JSX.Element => {
   return (
     <>
       <Container>
         <Card
-          shadow="shadow-md"
-          padding="p-4"
+          padding="px-4 pt-8"
+          shadow="shadow-none"
+          rounded="rounded-none"
           margin="mx-auto"
-          rounded="rounded-md"
+          height="h-screen"
         >
-          <h1>Login</h1>
+          <>
+            <Link href="/" passHref>
+              <a>
+                <Logo
+                  responsive={false}
+                  boxWidth="w-40"
+                  margin="mx-auto"
+                  padding="py-2"
+                  src="/assets/pictures/brand/vitary-logo.png"
+                  width={237}
+                  height={64}
+                />
+              </a>
+            </Link>
+          </>
+          <div className="mt-8">
+            <Heading level={3} margin="mb-2" size="normal">
+              Welcome back
+            </Heading>
+            <p className="mt-2 text-sm">Sign in with your user ID.</p>
+          </div>
         </Card>
       </Container>
     </>
