bb233c3141f28aabb984041e8a8fc265b1648737
Florian Rand <florian.rand@gmail.com>
2021-11-29 11:09:40 +0100

    Refactor logo to be responsive or fixed, add padding and margin

--- components/elements/Logo.tsx
+++ components/elements/Logo.tsx
@@ -1,13 +1,32 @@
+import classNames from "classnames";
 import Image from "next/image";
 interface IProps {
   src: string | StaticImageData;
+  boxWidth?: string;
+  margin?: string;
+  padding?: string;
   width: number | string;
   height: number | string;
+  responsive?: boolean;
 }
 
-const Logo = ({ src, width, height }: IProps): JSX.Element => {
+const Logo = ({
+  src,
+  boxWidth = "w-40",
+  margin,
+  padding,
+  width,
+  height,
+  responsive,
+}: IProps): JSX.Element => {
   return (
-    <div className="w-40 md:w-48 lg:w-56">
+    <div
+      className={classNames(
+        responsive ? "w-40 md:w-48 lg:w-56" : boxWidth,
+        margin,
+        padding
+      )}
+    >
       <Image
         layout="responsive"
         objectFit="contain"
