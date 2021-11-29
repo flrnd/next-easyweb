4ae1fc66f3d3c70a2bfd80de9cc386ff9e142880
Florian Rand <florian.rand@gmail.com>
2021-11-29 16:53:04 +0100

    Bulk sheet

--- api/login.ts
+++ api/login.ts
@@ -1,25 +1,22 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import { Client } from "pg";
+import { Pool } from "pg";
 
 export default async function (
   request: VercelRequest,
   res: VercelResponse
 ): Promise<VercelResponse> {
   const { username, password } = request.body;
-  const client = new Client();
+  const connectionString = process.env.DATABASE_URL;
 
   try {
-    client.connect();
-    const queryRes = await client.query(
+    const pgPool = new Pool({ connectionString });
+    const queryRes = pgPool.query(
       `SELECT website.authenticate_user(${username}, ${password})`
     );
-
     return res.status(200).json({
-      message: queryRes,
+      message: { queryRes },
     });
   } catch (err) {
     return res.status(500).json({ message: err.message });
-  } finally {
-    await client.end();
   }
 }
