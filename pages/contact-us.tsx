ad77ad68d5c06aa952ba9734c6afe267b95407e2
Florian Rand <florian.rand@gmail.com>
2021-11-29 12:07:42 +0100

    Add layout·

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -1,24 +1,26 @@
 import Image from "next/image";
 import Contact from "../components/elements/Contact";
-import { Container } from "../components/layout";
+import { Container, Layout } from "../components/layout";
 import { clinicImage, contactDataList } from "../test/__mocks__/fakeData";
 
 const ContactUs = (): JSX.Element => {
   return (
     <>
-      <main className="container mx-auto flex flex-col-reverse md:flex-row">
-        <Container size="max-w-lg" margin="mx-5">
-          <Contact
-            title="Contacto"
-            text="Para cualquier duda no dude en contactar con nosotros"
-            size="medium"
-            list={contactDataList}
-          />
-        </Container>
-        <div className="w-full mb-4 md:mb-0">
-          <Image src={clinicImage} layout="responsive" objectFit="contain" />
-        </div>
-      </main>
+      <Layout>
+        <main className="container mx-auto flex flex-col-reverse md:flex-row">
+          <Container size="max-w-lg" margin="mx-5">
+            <Contact
+              title="Contacto"
+              text="Para cualquier duda no dude en contactar con nosotros"
+              size="medium"
+              list={contactDataList}
+            />
+          </Container>
+          <div className="w-full mb-4 md:mb-0">
+            <Image src={clinicImage} layout="responsive" objectFit="contain" />
+          </div>
+        </main>
+      </Layout>
     </>
   );
 };
