abea08febae9b14c3c24c10b6df447f5f69d2c2c
Florian Rand <florian.rand@gmail.com>
2021-11-29 10:44:42 +0100

    Remove layour from _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,11 @@
 import type { AppProps } from "next/app";
-import { DefaultSeo } from "next-seo";
-import SEO from "../next-seo.config"; // Default Seo configuration options
 import "../styles/global.css";
-import { Layout, Header, Footer } from "../components/layout";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <Layout>
-      <DefaultSeo {...SEO} />
-      <Header />
+    <>
       <Component {...pageProps} />
-      <Footer />
-    </Layout>
+    </>
   );
 }
 
