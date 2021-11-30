7d786625fad843d1004d3d4f72bc5ac0a60830d2
Florian Rand <florian.rand@gmail.com>
2021-11-30 11:19:50 +0100

    Change placeholder data

--- containers/db/05-data.sql
+++ containers/db/05-data.sql
@@ -7,10 +7,10 @@
 
 WITH new_users AS (
   SELECT * FROM (VALUES
-  ('John', 'Doe', 'email@something.com', 'secret 1'),
-  ('Jane', 'Doe', 'jane@dot.com', 'secret 2'),
-  ('Mike', 'Donovan', 'mike@my.es', 'pdw123'),
-  ('Carles', 'Puig', 'carles@del.net', '12345')) AS t (first_name, last_name, email, password)
+  ('John', 'Doe', 'email@example.com', '12345'),
+  ('Jane', 'Doe', 'jane@example.com', '12345'),
+  ('Mike', 'Donovan', 'mike@example.com', '12345'),
+  ('Carles', 'Puig', 'carles@example.com', '12345')) AS t (first_name, last_name, email, password)
 )
 SELECT website.register_user(first_name, last_name, email, password) FROM new_users;
 
