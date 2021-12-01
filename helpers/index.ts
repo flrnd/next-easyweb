67ce2dc9d755778695d1bc673de8ad66dfe0ecfa
Florian Rand <florian.rand@gmail.com>
2021-12-01 10:20:58 +0100

    remove helpers

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,13 +1,5 @@
 import calculateGridColumns from "./calculateGridColumns";
 import createNavigationList from "./createNavigationList";
 import stringToId from "./stringToId";
-import parseAuthPayload from "./parseAuthPayload";
-import timestampToSeconds from "./timestampToSeconds";
 
-export {
-  calculateGridColumns,
-  createNavigationList,
-  stringToId,
-  parseAuthPayload,
-  timestampToSeconds,
-};
+export { calculateGridColumns, createNavigationList, stringToId };
