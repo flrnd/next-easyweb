077c566b0f0732ee2ff028fd56d42b273746c2e2
Florian Rand <florian.rand@gmail.com>
2021-12-02 12:46:50 +0100

    WIP Account page

--- pages/user/account.tsx
+++ pages/user/account.tsx
@@ -1,12 +1,146 @@
+import { Session } from "@supabase/gotrue-js";
+import { useEffect, useState } from "react";
+import { Card } from "../../components/elements";
+import { Container } from "../../components/layout";
+import { supabase } from "../../lib/util/supabaseClient";
 import { useUser } from "../../lib/util/useUser";
 
-const Account = (): JSX.Element => {
-  const { user } = useUser();
+interface IProps {
+  session: Session;
+}
+
+const Account = ({ session }: IProps): JSX.Element => {
+  const [firstName, setFirstName] = useState(null);
+  const [lastName, setLastName] = useState(null);
+  const [billingAddress, setBillingAddress] = useState(null);
+  const [avatarUrl, setAvatarUrl] = useState(null);
+  const { getProfileDetails } = useUser();
+
+  useEffect(() => {
+    getProfile();
+  }, [session]);
+
+  async function getProfile() {
+    try {
+      const user = supabase.auth.user();
+
+      const { data, error, status } = await getProfileDetails({
+        userId: user.id,
+      });
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setFirstName(data.first_name);
+        setLastName(data.last_name);
+        setBillingAddress(data.billing_address);
+        setAvatarUrl(data.avatar_url);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  async function updateProfile({
+    first_name,
+    last_name,
+    billing_address,
+    avatar_url,
+  }) {
+    try {
+      const user = supabase.auth.user();
+
+      const updates = {
+        id: user.id,
+        first_name,
+        last_name,
+        billing_address,
+        avatar_url,
+        updated_at: new Date(),
+      };
+
+      const { error } = await supabase
+        .from("profiles")
+        .upsert(updates, { returning: "minimal" });
+
+      if (error) {
+        throw error;
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
   return (
-    <div>
-      <h1>Account</h1>
-      {user.email}
-    </div>
+    <Container>
+      <Card
+        padding="px-4 pt-8"
+        shadow="shadow-none"
+        rounded="rounded-none"
+        margin="mx-auto"
+        height="h-screen"
+      >
+        <label htmlFor="email">Email</label>
+        {session.user.email}
+        <div className="mt-8">
+          <label htmlFor="first_name">First Name</label>
+          <input
+            id="first_name"
+            type="text"
+            value={firstName || ""}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+        <div className="mt-8">
+          <label htmlFor="last_name">Last Name</label>
+          <input
+            id="last_name"
+            type="text"
+            value={lastName || ""}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div className="mt-8">
+          <label htmlFor="billing_address">Billing Address</label>
+          <input
+            id="billing_address"
+            type="text"
+            value={billingAddress || ""}
+            onChange={(e) => setBillingAddress(e.target.value)}
+          />
+        </div>
+        <div className="mt-8">
+          <label htmlFor="avatar_url">Avatar URL</label>
+          <input
+            id="avatar_url"
+            type="text"
+            value={avatarUrl || ""}
+            onChange={(e) => setAvatarUrl(e.target.value)}
+          />
+        </div>
+        <button
+          className="button block primary"
+          onClick={() =>
+            updateProfile({
+              first_name: firstName,
+              last_name: lastName,
+              billing_address: billingAddress,
+              avatar_url: avatarUrl,
+            })
+          }
+        >
+          Update profile
+        </button>
+        <div className="mt-8">
+          <button
+            className="button block"
+            onClick={() => supabase.auth.signOut()}
+          >
+            Sign Out
+          </button>
+        </div>
+      </Card>
+    </Container>
   );
 };
 
