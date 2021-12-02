b7d0a9233a0723be99cf7fd8af4c01d9af95f750
Florian Rand <florian.rand@gmail.com>
2021-12-02 14:04:20 +0100

    Sanitize index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,8 @@
-import { useEffect, useState } from "react";
-import { supabase } from "../lib/util/supabaseClient";
+import { useUser } from "../lib/util/useUser";
 import Example from "./example";
 
 export default function Index(): JSX.Element {
-  const [session, setSession] = useState(null);
-
-  useEffect(() => {
-    setSession(supabase.auth.session());
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      {
-        setSession(session);
-      }
-    });
-  }, []);
-
+  const { session } = useUser();
   return (
     <>
       {session && session.user.email}
