f230adae2d3a1ea01d8ee507919a03944fcd87dc
Florian Rand <florian.rand@gmail.com>
2021-12-09 11:44:05 +0100

    Update conditional header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,33 +1,34 @@
-//import Link from "next/link";
-//import { siteMenu } from "../../test/__mocks__/fakeData";
-//import { Logo } from "../elements";
-//import { BurgerMenu, NavigationBar } from "../navigation";
+import Link from "next/link";
+import { siteMenu } from "../../test/__mocks__/fakeData";
+import { Logo } from "../elements";
+import { BurgerMenu, NavigationBar } from "../navigation";
+import { useUser } from "../../lib/util/useUser";
 import NavigationWithLogin from "../navigation/NavigationWithLogin";
 
-//const Header = (): JSX.Element => {
-//  return (
-//    <div className="container mx-auto">
-//      <NavigationBar>
-//        <Link href="/" passHref>
-//          <a>
-//            <Logo
-//              src="/assets/pictures/brand/vitary-logo.png"
-//              width={237}
-//              height={64}
-//            />
-//          </a>
-//        </Link>
-//
-//        <BurgerMenu menu={siteMenu} />
-//      </NavigationBar>
-//    </div>
-//  );
-//};
-//
-//export default Header;
-
 const Header = (): JSX.Element => {
-  return <NavigationWithLogin />;
+  const { session } = useUser();
+
+  if (session) {
+    return <NavigationWithLogin />;
+  } else {
+    return (
+      <div className="container mx-auto">
+        <NavigationBar>
+          <Link href="/" passHref>
+            <a>
+              <Logo
+                src="/assets/pictures/brand/vitary-logo.png"
+                width={237}
+                height={64}
+              />
+            </a>
+          </Link>
+
+          <BurgerMenu menu={siteMenu} />
+        </NavigationBar>
+      </div>
+    );
+  }
 };
 
 export default Header;
