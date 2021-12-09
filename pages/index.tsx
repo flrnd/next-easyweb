b85848231a82c12d88541dec9e9daa961a190f2b
Florian Rand <florian.rand@gmail.com>
2021-12-09 12:03:57 +0100

    Clean

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
-import { useUser } from "../lib/util/useUser";
 import Example from "./example";
 
 export default function Index(): JSX.Element {
-  const { session } = useUser();
   return (
     <>
-      {session && session.user.email}
       <Example />
     </>
   );
