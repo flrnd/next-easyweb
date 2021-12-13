0e5ebac0f52611d4e1985436c01c2dacd5de9870
Florian Rand <florian.rand@gmail.com>
2021-12-13 16:33:13 +0100

    Style dashboard navbar

--- components/navigation/NavigationWithLogin.tsx
+++ components/navigation/NavigationWithLogin.tsx
@@ -77,7 +77,7 @@ const NavigationWithLogin = (): JSX.Element => {
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <button
               type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              className="inline-flex items-center justify-center p-2 rounded-md text-indigo-900 hover:text-white hover:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
               onClick={() => setMobileMenuIsOpen(!mobileMenuIsOpen)}
@@ -160,7 +160,7 @@ const NavigationWithLogin = (): JSX.Element => {
               <div>
                 <button
                   type="button"
-                  className="h-6 w-6 flex text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white p-1"
+                  className="h-6 w-6 flex text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white p-1 text-indigo-900"
                   id="user-menu-button"
                   aria-expanded={userMenuIsOpen ? "true" : "false"}
                   aria-haspopup="true"
