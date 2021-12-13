3616c833c762513586266513d9a2f32077e67baa
Florian Rand <florian.rand@gmail.com>
2021-12-13 16:32:44 +0100

    Cleanup profile

--- components/dashboard/profile.tsx
+++ components/dashboard/profile.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Button } from "../controls";
-import { Card } from "../elements";
 import { supabase } from "../../lib/util/supabaseClient";
 import { useUser } from "../../lib/store/useUser";
 import { IProfileData } from "../../types/interfaces";
-import profilePicture from "../../public/assets/placeholder/profile.png";
+import { Heading } from "../typography";
 
 const ProfileContainer = (): JSX.Element => {
   const [firstName, setFirstName] = useState(null);
@@ -97,112 +95,88 @@ const ProfileContainer = (): JSX.Element => {
   };
 
   return (
-    <div className="container mt-10 mx-auto">
-      <Card
-        padding="p-8"
-        shadow="shadow-sm"
-        rounded="rounded-md"
-        margin="mx-auto"
-        width="max-w-2xl"
-        height="h-full"
-      >
-        <div className="flex flex-col sm:flex-row mt-10">
-          <div className="mr-5">
-            <Image
-              src={profilePicture}
-              width={240}
-              height={240}
-              alt="placeholder"
-            />
-            <input
-              className="appearance-none hiden w-full py-2 text-grey-darker"
-              id="avatar"
-              value={avatarUrl || ""}
-              type="text"
-              {...register("avatar", { required: false })}
-              placeholder="Avatar URL"
-              onChange={(e) => setAvatarUrl(e.target.value)}
-            />
-          </div>
-          <div className="w-full">
-            {user?.email && <div className="font-bold mb-5">{user?.email}</div>}
-            <div className="mb-4">
-              <label className="block text-grey-darker text-sm font-bold mb-2">
-                First name
-                {errors.firstName && (
-                  <span className="text-sm text-red-500 font-normal italic ml-4">
-                    * Missing First name
-                  </span>
-                )}
-              </label>
-              <input
-                className="appearance-none inline w-full py-2 text-grey-darker"
-                id="first_name"
-                value={firstName || ""}
-                type="text"
-                {...register("firstName", { required: false })}
-                placeholder="Jane"
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-grey-darker text-sm font-bold mb-2">
-                Last name
-                {errors.firstName && (
-                  <span className="text-sm text-red-500 font-normal italic ml-4">
-                    * Missing Last name
-                  </span>
-                )}
-              </label>
-              <input
-                className="appearance-none inline w-full py-2 text-grey-darker"
-                id="last_name"
-                value={lastName || ""}
-                type="text"
-                {...register("lastName", { required: false })}
-                placeholder="Doe"
-                onChange={(e) => setLastName(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-grey-darker text-sm font-bold mb-2">
-                Billing Address
-                {errors.billingAddress && (
-                  <span className="text-sm text-red-500 font-normal italic ml-4">
-                    * Missing Billing Address
-                  </span>
-                )}
-              </label>
-              <input
-                className="appearance-none inline w-full py-2 text-grey-darker"
-                id="billing_address"
-                value={billingAddress || ""}
-                type="text"
-                {...register("billingAddress", { required: false })}
-                placeholder="Sesame Street 5, corner square 44 street"
-                onChange={(e) => setBillingAddress(e.target.value)}
-              />
-            </div>
-            <div className="mt-8 flex mb-8">
-              <Button
-                background="bg-indigo-600"
-                textColor="text-white"
-                rounded="rounded-none"
-                shadow="shadow-md"
-                margin="mr-4"
-                hoverBg="bg-indigo-700"
-                onClick={handleSubmit(onSubmit)}
-                label="Update profile"
-              />
-            </div>
-            {message.type === "success" && (
-              <span className="text-sm text-green-500 font-normal italic ml-4">
-                {message.content}
+    <div className="mt-10">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="border-b-2">
+          <Heading level={4} size="medium" weight="font-bold">
+            Profile
+          </Heading>
+        </div>
+        <div className="my-4">
+          <label className="block text-grey-darker text-sm font-bold mb-2">
+            First name
+            {errors.firstName && (
+              <span className="text-sm text-red-500 font-normal italic ml-4">
+                * Missing First name
               </span>
             )}
-          </div>
+          </label>
+          <input
+            className="appearance-none inline w-full py-2 text-grey-darker"
+            id="first_name"
+            value={firstName || ""}
+            type="text"
+            {...register("firstName", { required: false })}
+            placeholder="Jane"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
         </div>
-      </Card>
+        <div className="mb-4">
+          <label className="block text-grey-darker text-sm font-bold mb-2">
+            Last name
+            {errors.firstName && (
+              <span className="text-sm text-red-500 font-normal italic ml-4">
+                * Missing Last name
+              </span>
+            )}
+          </label>
+          <input
+            className="appearance-none inline w-full py-2 text-grey-darker"
+            id="last_name"
+            value={lastName || ""}
+            type="text"
+            {...register("lastName", { required: false })}
+            placeholder="Doe"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-grey-darker text-sm font-bold mb-2">
+            Billing Address
+            {errors.billingAddress && (
+              <span className="text-sm text-red-500 font-normal italic ml-4">
+                * Missing Billing Address
+              </span>
+            )}
+          </label>
+          <input
+            className="appearance-none inline w-full py-2 text-grey-darker"
+            id="billing_address"
+            value={billingAddress || ""}
+            type="text"
+            {...register("billingAddress", { required: false })}
+            placeholder="Sesame Street 5, corner square 44 street"
+            onChange={(e) => setBillingAddress(e.target.value)}
+          />
+        </div>
+        <div className="mt-8 flex mb-8">
+          <Button
+            background="bg-indigo-600"
+            textColor="text-white"
+            rounded="rounded-none"
+            shadow="shadow-md"
+            margin="mr-4"
+            hoverBg="bg-indigo-700"
+            onClick={handleSubmit(onSubmit)}
+            label="Update profile"
+          />
+        </div>
+        {message.type === "success" && (
+          <span className="text-sm text-green-500 font-normal italic ml-4">
+            {message.content}
+          </span>
+        )}
+      </div>
     </div>
   );
 };
