34099940f80d360d5c6dd648cc7fc464e9b68c2d
Florian Rand <florian.rand@gmail.com>
2021-12-13 15:09:07 +0100

    Remove from import

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -1,7 +1,5 @@
-import Header from "./Header";
-import Footer from "./Footer";
 import Layout from "./Layout";
 import Container from "./Container";
 import Grid from "./Grid";
 
-export { Header, Footer, Layout, Grid, Container };
+export { Layout, Grid, Container };
