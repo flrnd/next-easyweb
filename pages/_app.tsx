a4085873558a83e41ef3a6f9583ae80eb3343c8a
Florian Rand <florian.rand@gmail.com>
2021-12-14 14:09:29 +0100

    Update import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
-import { UserContextProvider } from "../lib/store/hooks/useUser";
+import { UserContextProvider } from "../lib/store/context/UserContextProvider";
+
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
