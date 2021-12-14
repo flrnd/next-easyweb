336045648135cf87d2459b0a07bab0eee3afbb2b
Florian Rand <florian.rand@gmail.com>
2021-12-14 14:21:33 +0100

    Remove context from test

--- components/navigation/BurgerMenu/index.test.tsx
+++ components/navigation/BurgerMenu/index.test.tsx
@@ -1,26 +1,17 @@
 import { render } from "@testing-library/react";
 import { BurgerMenu } from "..";
-import { UserContextProvider } from "../../../lib/store/context/UserContextProvider";
 
 describe("Burger Menu", () => {
   const menu = ["services", "about", "contact us"];
 
   it("renders mobile and desktop menus", () => {
-    const { getAllByRole } = render(
-      <UserContextProvider>
-        <BurgerMenu menu={menu} />
-      </UserContextProvider>
-    );
+    const { getAllByRole } = render(<BurgerMenu menu={menu} />);
 
     expect(getAllByRole("listitem")).toHaveLength(menu.length * 2);
   });
 
   it("render a button", () => {
-    const { getByTitle, getAllByRole } = render(
-      <UserContextProvider>
-        <BurgerMenu menu={menu} />
-      </UserContextProvider>
-    );
+    const { getByTitle, getAllByRole } = render(<BurgerMenu menu={menu} />);
 
     expect(getByTitle("menu")).toBeInTheDocument();
     expect(getAllByRole("listitem")).toHaveLength(menu.length * 2);
