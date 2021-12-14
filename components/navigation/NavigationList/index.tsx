bde58438c6bffe571fd87f8cbd99e325a54df3d3
Florian Rand <florian.rand@gmail.com>
2021-12-14 12:00:47 +0100

    Add button

--- components/navigation/NavigationList/index.tsx
+++ components/navigation/NavigationList/index.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import { NavigationListItem } from "..";
 import { createNavigationList } from "../../../lib/util";
+import { Button } from "../../controls";
 
 interface Props {
   isVertical?: boolean;
@@ -10,15 +11,27 @@ const NavigationList = ({ isVertical = false, list }: Props): JSX.Element => {
   const navigationList = createNavigationList(list);
 
   return (
-    <ul className={classNames("flex", isVertical && "flex-col")}>
-      {navigationList.map((navItem) => (
-        <NavigationListItem
-          key={navItem.name}
-          item={navItem}
-          margin={!isVertical && "ml-4"}
-        />
-      ))}
-    </ul>
+    <div className="flex items-center">
+      <ul className={classNames("flex", isVertical && "flex-col")}>
+        {navigationList.map((navItem) => (
+          <NavigationListItem
+            key={navItem.name}
+            item={navItem}
+            margin={!isVertical && "ml-4"}
+          />
+        ))}
+      </ul>
+      <Button
+        background="bg-indigo-600"
+        textColor="text-white"
+        rounded="rounded-md"
+        shadow="shadow-md"
+        margin="ml-4"
+        hoverBg="bg-indigo-700"
+        href="/signin"
+        label="sign in"
+      />
+    </div>
   );
 };
 
