9c342b32afb3f8507a114da2e56fcc78c334f58a
Florian Rand <florian.rand@gmail.com>
2021-12-14 12:35:17 +0100

    Remove from header

--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -1,34 +1,26 @@
 import Link from "next/link";
 import { siteMenu } from "../../../__mocks__/fakeData/data";
-import { useUser } from "../../../lib/store/hooks/useUser";
 import { BurgerMenu, NavigationBar } from "../../navigation";
 import { Logo } from "../../elements";
-import DashboardNavigationBar from "../../dashboard/DashboardNavigationBar";
 
 const Header = (): JSX.Element => {
-  const { session } = useUser();
+  return (
+    <div className="container mx-auto">
+      <NavigationBar>
+        <Link href="/" passHref>
+          <a>
+            <Logo
+              src="/assets/pictures/brand/vitary-logo.png"
+              width={237}
+              height={64}
+            />
+          </a>
+        </Link>
 
-  if (session) {
-    return <DashboardNavigationBar />;
-  } else {
-    return (
-      <div className="container mx-auto">
-        <NavigationBar>
-          <Link href="/" passHref>
-            <a>
-              <Logo
-                src="/assets/pictures/brand/vitary-logo.png"
-                width={237}
-                height={64}
-              />
-            </a>
-          </Link>
-
-          <BurgerMenu menu={siteMenu} />
-        </NavigationBar>
-      </div>
-    );
-  }
+        <BurgerMenu menu={siteMenu} />
+      </NavigationBar>
+    </div>
+  );
 };
 
 export default Header;
