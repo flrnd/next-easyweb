2f8ed95eff1533906b9bcd93acc5a1ca9d3cb293
Florian Rand <florian.rand@gmail.com>
2021-12-14 12:35:42 +0100

    If session shows dasboard button

--- components/navigation/NavigationList/index.tsx
+++ components/navigation/NavigationList/index.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import { NavigationListItem } from "..";
+import { useUser } from "../../../lib/store/hooks/useUser";
 import { createNavigationList } from "../../../lib/util";
 import { Button } from "../../controls";
 
@@ -9,6 +10,7 @@ interface Props {
 }
 const NavigationList = ({ isVertical = false, list }: Props): JSX.Element => {
   const navigationList = createNavigationList(list);
+  const { session } = useUser();
 
   return (
     <div className="flex items-center">
@@ -21,16 +23,29 @@ const NavigationList = ({ isVertical = false, list }: Props): JSX.Element => {
           />
         ))}
       </ul>
-      <Button
-        background="bg-indigo-600"
-        textColor="text-white"
-        rounded="rounded-md"
-        shadow="shadow-md"
-        margin="ml-4"
-        hoverBg="bg-indigo-700"
-        href="/signin"
-        label="sign in"
-      />
+      {session ? (
+        <Button
+          background="bg-indigo-600"
+          textColor="text-white"
+          rounded="rounded-md"
+          shadow="shadow-md"
+          margin="ml-4"
+          hoverBg="bg-indigo-700"
+          href="/dashboard/profile"
+          label="Dashboard"
+        />
+      ) : (
+        <Button
+          background="bg-indigo-600"
+          textColor="text-white"
+          rounded="rounded-md"
+          shadow="shadow-md"
+          margin="ml-4"
+          hoverBg="bg-indigo-700"
+          href="/signin"
+          label="sign in"
+        />
+      )}
     </div>
   );
 };
