94d5be811ae111384f60c781a4b06d5730539344
Florian Rand <florian.rand@gmail.com>
2021-12-14 12:35:59 +0100

    Add header and footer to index

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -1,5 +1,7 @@
 import DefaultLayout from "./DefaultLayout";
 import Container from "./Container";
+import Footer from "./Footer";
 import Grid from "./Grid";
+import Header from "./Header";
 
-export { DefaultLayout, Grid, Container };
+export { DefaultLayout, Container, Footer, Grid, Header };
