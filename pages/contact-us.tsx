8fbbffda1362f5c1ed08aeb90c78dc1d96edd971
Florian Rand <florian.rand@gmail.com>
2021-12-14 11:01:43 +0100

    Update page

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Contact from "../components/elements/Contact";
-import { Container, Layout } from "../components/layout";
+import { Container, DefaultLayout } from "../components/layout";
 import { clinicImage, contactDataList } from "../__mocks__/fakeData";
 
 const ContactUs = (): JSX.Element => {
   return (
     <>
-      <Layout>
+      <DefaultLayout>
         <main className="container mx-auto flex flex-col-reverse md:flex-row">
           <Container size="max-w-lg" margin="mx-5">
             <Contact
@@ -20,7 +20,7 @@ const ContactUs = (): JSX.Element => {
             <Image src={clinicImage} layout="responsive" objectFit="contain" />
           </div>
         </main>
-      </Layout>
+      </DefaultLayout>
     </>
   );
 };
