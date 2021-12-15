3d539b30900b4fc66431ae2ed6e932ce3d567061
Florian Rand <florian.rand@gmail.com>
2021-12-15 10:30:50 +0100

    Add error messages and tests

--- lib/util/index.ts
+++ lib/util/index.ts
@@ -27,13 +27,54 @@ const createNavigationList = (menu: string[]): INavigationListItem[] =>
     }
   });
 
-const validatePasswordStrength = (password: string): boolean => {
+const validatePasswordStrength = (
+  password: string
+): { validation: boolean; errors: string[] } => {
+  if (!password) return { validation: false, errors: ["empty password"] };
+
+  const messages = {
+    lowerCase: "Must contain at least one lowercase letter",
+    upperCase: "Must contain at least one uppercase letter",
+    number: "Must contain at least one number",
+    length: "Must be at least 8 characters long",
+    symbol: "Must contain at least one symbol character",
+  };
+
+  const errors = [];
+
+  const hasLength = password.length >= 8;
   const hasLowerCase = /[a-z]/.test(password);
   const hasUpperCase = /[A-Z]/.test(password);
   const hasNumber = /[0-9]/.test(password);
   const hasSymbols = /[!@#$%^&*()--__+.]/.test(password);
 
-  return hasLowerCase && hasUpperCase && hasNumber && hasSymbols;
+  if (!hasLength) {
+    errors.push(messages.length);
+  }
+
+  if (!hasLowerCase) {
+    errors.push(messages.lowerCase);
+  }
+
+  if (!hasUpperCase) {
+    errors.push(messages.upperCase);
+  }
+
+  if (!hasNumber) {
+    errors.push(messages.number);
+  }
+
+  if (!hasSymbols) {
+    errors.push(messages.symbol);
+  }
+
+  const validation =
+    hasLength && hasLowerCase && hasUpperCase && hasNumber && hasSymbols;
+
+  return {
+    validation,
+    errors,
+  };
 };
 
 export {
