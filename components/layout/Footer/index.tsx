27eaef902f7bfc8e4ab293905bb475061a050f90
Florian Rand <florian.rand@gmail.com>
2021-12-15 13:36:36 +0100

    Change about/pricing spanish to engrish

--- components/layout/Footer/index.tsx
+++ components/layout/Footer/index.tsx
@@ -2,7 +2,7 @@ import { companySocial, logotype } from "../../../__mocks__/fakeData/data";
 import { Logo, SocialIconList } from "../../elements";
 import { NavigationItemList } from "../../navigation";
 
-const list = ["services", "about", "contact us"];
+const list = ["services", "pricing", "contact us"];
 const hours = ["Mon-Fri: 9am-5pm", "Sat: 10am-2pm", "Sun: Closed"];
 const contact = ["Email: hello@example.com", "Phone: 123-456-789"];
 const year = new Date().getFullYear();
@@ -30,11 +30,11 @@ const Footer = (): JSX.Element => {
           </div>
 
           <div className="footer-section">
-            <div className="text-2xl mb-2">Enlaces</div>
+            <div className="text-2xl mb-2">Links</div>
             <NavigationItemList isVertical={true} list={list} />
           </div>
           <div className="footer-section">
-            <div className="text-2xl mb-2">Horario</div>
+            <div className="text-2xl mb-2">Hours</div>
             <div>
               {hours.map((h) => (
                 <div key={h}>{h}</div>
@@ -42,7 +42,7 @@ const Footer = (): JSX.Element => {
             </div>
           </div>
           <div className="footer-section">
-            <div className="text-2xl mb-2">Contacto</div>
+            <div className="text-2xl mb-2">Contact</div>
             <div>
               {contact.map((m) => (
                 <div key={m}>{m}</div>
@@ -52,8 +52,8 @@ const Footer = (): JSX.Element => {
         </div>
       </div>
       <div className="bg-indigo-800 text-center text-white py-2">
-        {year} &copy; Vitary. All rights reserved. Designed by s&infin;mos
-        digital
+        {year} &copy; Vitary. All rights reserved. Designed by Espinete & Don
+        Pinpon digital
       </div>
     </footer>
   );
