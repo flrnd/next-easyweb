842df1c91b4939512f9812ffd77053f07d955211
Florian Rand <florian.rand@gmail.com>
2021-12-15 13:36:14 +0100

    Remove about for pricing

--- pages/pricing/index.tsx
+++ pages/pricing/index.tsx
@@ -3,14 +3,14 @@ import { DefaultLayout } from "../../components/layout";
 import Footer from "../../components/layout/Footer";
 import Header from "../../components/layout/Header";
 
-const About = (): JSX.Element => {
+const Pricing = (): JSX.Element => {
   return (
     <>
       <DefaultLayout>
         <NextSeo title="This is a title" description="this is a description" />
         <Header />
         <main className="py-6">
-          <h1>About</h1>
+          <h1>Pricing</h1>
         </main>
         <Footer />
       </DefaultLayout>
@@ -18,4 +18,4 @@ const About = (): JSX.Element => {
   );
 };
 
-export default About;
+export default Pricing;
