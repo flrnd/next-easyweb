ab592317ebe09c555e6d12a82928883c54574519
Florian Rand <florian.rand@gmail.com>
2021-12-17 15:56:57 +0100

    Refactor with interface

--- lib/store/hooks/useNotification.tsx
+++ lib/store/hooks/useNotification.tsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
+import { INotificationMessage } from "../../types";
 
 const useNotification = (): any => {
   const [showNotification, setShowNotification] = useState(false);
-  const [notificationMessage, setNotificationMessage] = useState({
-    type: "",
-    content: "",
-  });
+  const [notificationMessage, setNotificationMessage] =
+    useState<INotificationMessage>({
+      type: "",
+      content: "",
+    });
 
-  const notification = (
-    message: { type: string; content: string },
-    time = 5000
-  ) => {
+  const notification = (message: INotificationMessage, time = 5000) => {
     setShowNotification(true);
     setNotificationMessage(message);
     setTimeout(() => {
