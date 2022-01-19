bd19425f0f840e43db5e061123c672558297a1d3
Florian Rand <florian.rand@gmail.com>
2022-01-19 10:43:53 +0100

    Add landing layout

---
+++ components/layout/LandingLayout/index.tsx
@@ -0,0 +1,43 @@
+import Head from "next/head";
+import { DefaultSeo } from "next-seo";
+import { ReactNode } from "react";
+import { motion } from "framer-motion";
+
+import SEO from "../../../next-seo.config";
+
+interface Props {
+  children: ReactNode;
+}
+
+const LandingLayout = ({ children }: Props): JSX.Element => {
+  const variants = {
+    hidden: { opacity: 0, x: -200, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: -100 },
+  };
+  return (
+    <div className="layout">
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/sentient/Sentient-Variable.ttf"
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
+      <DefaultSeo {...SEO} />
+      <motion.main
+        className="py-6"
+        variants={variants}
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        transition={{ type: "linear" }}
+      >
+        {children}
+      </motion.main>
+    </div>
+  );
+};
+
+export default LandingLayout;
