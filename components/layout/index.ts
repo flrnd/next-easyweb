653d0e6ee7e03cc84a022bad5311dd5f45a1aad0
Florian Rand <florian.rand@gmail.com>
2022-01-19 10:41:35 +0100

    Add other layouts

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -1,7 +1,8 @@
 import DefaultLayout from "./DefaultLayout";
+import LandingLayout from "./LandingLayout";
 import Container from "./Container";
 import Footer from "./Footer";
 import Grid from "./Grid";
 import Header from "./Header";
 
-export { DefaultLayout, Container, Footer, Grid, Header };
+export { DefaultLayout, LandingLayout, Container, Footer, Grid, Header };
