646b1ea35b0394be9dbea9725eaaefecae7d4029
Florian Rand <florian.rand@gmail.com>
2022-01-19 10:41:50 +0100

    Animate burger menu

--- components/navigation/BurgerMenu/index.tsx
+++ components/navigation/BurgerMenu/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { NavigationItemList } from "..";
 import { Button } from "../../controls";
+import { motion } from "framer-motion";
 
 interface IProps {
   menu: string[];
@@ -10,6 +11,16 @@ interface IProps {
 const BurgerMenu = ({ menu }: IProps): JSX.Element => {
   const [isOpen, setIsOpen] = useState(false);
   const handleClick = (): void => setIsOpen(!isOpen);
+
+  const modalContentVariants = {
+    open: { opacity: 1, y: 0 },
+    closed: { opacity: 0, y: "-100%" },
+  };
+
+  const modalVariants = {
+    open: { opacity: 1 },
+    closed: { opacity: 0 },
+  };
 
   return (
     <>
@@ -26,22 +37,37 @@ const BurgerMenu = ({ menu }: IProps): JSX.Element => {
         </Button>
       </div>
 
-      <div className={isOpen ? "modal" : "hidden"} onClick={handleClick}>
-        <div className="modal-content">
-          <div className="medium font-bold mb-5">Menu</div>
+      <motion.div
+        className={isOpen ? "modal" : "hidden"}
+        onClick={handleClick}
+        animate={isOpen ? "open" : "closed"}
+        transition={{ duration: 0.2 }}
+        variants={modalVariants}
+      >
+        <motion.div
+          className="modal-content"
+          animate={isOpen ? "open" : "closed"}
+          transition={{ duration: 0.2 }}
+          variants={modalContentVariants}
+        >
+          <div className="medium font-bold mb-5">
+            <Link href="/" passHref>
+              <a>Inicio</a>
+            </Link>
+          </div>
           <NavigationItemList list={menu} isVertical={isOpen} />
           <Link href="/signin" passHref>
-            <a className="medium font-bold text-indigo-500 hover:text-indigo-800">
-              Login
+            <a className="p-4 font-bold text-indigo-500 hover:text-indigo-800">
+              login
             </a>
           </Link>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
       <div className="nav-menu">
         <NavigationItemList list={menu} />
         <Link href="/signin" passHref>
           <a className="p-4 font-bold text-indigo-500 hover:text-indigo-800">
-            Login
+            login
           </a>
         </Link>
       </div>
