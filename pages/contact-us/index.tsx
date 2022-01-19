72f327456c560230a1006f1416afc4bd96ae02e1
Florian Rand <florian.rand@gmail.com>
2022-01-19 10:40:09 +0100

    Remove header and footer from page

--- pages/contact-us/index.tsx
+++ pages/contact-us/index.tsx
@@ -1,15 +1,12 @@
 import Image from "next/image";
 import Contact from "../../components/elements/Contact";
 import { Container, DefaultLayout } from "../../components/layout";
-import Footer from "../../components/layout/Footer";
-import Header from "../../components/layout/Header";
 import { clinicImage, contactDataList } from "../../__mocks__/fakeData/data";
 
 const ContactUs = (): JSX.Element => {
   return (
     <>
       <DefaultLayout>
-        <Header />
         <main className="container mx-auto flex flex-col-reverse md:flex-row">
           <Container size="max-w-lg" margin="mx-5">
             <Contact
@@ -23,7 +20,6 @@ const ContactUs = (): JSX.Element => {
             <Image src={clinicImage} layout="responsive" objectFit="contain" />
           </div>
         </main>
-        <Footer />
       </DefaultLayout>
     </>
   );
