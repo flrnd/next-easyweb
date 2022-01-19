4e5f8376be44f9edb7c4fe7826e0a1ee46e3b2ad
Florian Rand <florian.rand@gmail.com>
2022-01-19 10:40:26 +0100

    Add header and footer to layout

--- components/layout/DefaultLayout/index.tsx
+++ components/layout/DefaultLayout/index.tsx
@@ -1,5 +1,8 @@
+import Head from "next/head";
 import { DefaultSeo } from "next-seo";
 import { ReactNode } from "react";
+import { motion } from "framer-motion";
+import { Footer, Header } from "..";
 
 import SEO from "../../../next-seo.config";
 
@@ -8,10 +11,34 @@ interface Props {
 }
 
 const DefaultLayout = ({ children }: Props): JSX.Element => {
+  const variants = {
+    hidden: { opacity: 0, x: -200, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: -100 },
+  };
   return (
     <div className="layout">
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/sentient/Sentient-Variable.ttf"
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
       <DefaultSeo {...SEO} />
-      {children}
+      <Header />
+      <motion.main
+        className="py-6 px-2 md:px-0"
+        variants={variants}
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        transition={{ type: "linear" }}
+      >
+        {children}
+      </motion.main>
+      <Footer />
     </div>
   );
 };
