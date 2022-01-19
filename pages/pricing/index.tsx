58ff78fb5efd4058648a20457532adc10ff4634e
Florian Rand <florian.rand@gmail.com>
2022-01-19 10:40:09 +0100

    Remove header and footer from page

--- pages/pricing/index.tsx
+++ pages/pricing/index.tsx
@@ -1,18 +1,15 @@
 import { NextSeo } from "next-seo";
 import { DefaultLayout } from "../../components/layout";
-import Footer from "../../components/layout/Footer";
-import Header from "../../components/layout/Header";
 
 const Pricing = (): JSX.Element => {
   return (
     <>
       <DefaultLayout>
         <NextSeo title="This is a title" description="this is a description" />
-        <Header />
+
         <main className="py-6">
           <h1>Pricing</h1>
         </main>
-        <Footer />
       </DefaultLayout>
     </>
   );
