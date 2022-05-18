6b8f3006ed9b0283bb483335ebe7d46ea6f9da76
Florian Rand <florian.rand@gmail.com>
2022-05-18 16:34:39 +0200

    Add login form test wip

---
+++ components/form/LoginForm/index.test.tsx
@@ -0,0 +1,48 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import LoginForm from ".";
+import { IFormData } from "../../../lib/types";
+
+const setup = () => {
+  const formData: IFormData = {
+    username: "Espinete",
+    password: "My-Super-Secret",
+  };
+
+  const mockSubmit = jest.fn((formData) => {
+    return {
+      username: formData.username,
+      password: formData.password,
+    };
+  });
+
+  const utils = render(
+    <LoginForm onSubmit={mockSubmit} submitLabel="submit" />
+  );
+  const username = utils.getByLabelText("username-input");
+  const password = utils.getByLabelText("password-input");
+  const button = utils.getByLabelText("submit-button");
+
+  return {
+    formData,
+    mockSubmit,
+    username,
+    password,
+    button,
+    ...utils,
+  };
+};
+
+describe("Login form", () => {
+  test("It should send correct username and password", async () => {
+    const { formData, mockSubmit, username, password, button } = setup();
+    fireEvent.input(username, { target: { value: formData.username } });
+    fireEvent.input(password, { target: { value: formData.password } });
+    fireEvent.submit(button);
+
+    await waitFor(() => expect(screen.queryAllByRole("alert")).toHaveLength(0));
+    expect(mockSubmit).toBeCalledWith({
+      username: formData.username,
+      password: formData.password,
+    });
+  });
+});
