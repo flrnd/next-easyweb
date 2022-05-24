b03c3691c5e7e5bcc1b20a38e620e7a27de3ec5c
Florian Rand <florian.rand@gmail.com>
2022-05-24 14:45:48 +0200

    Add aria label and refactor tests

--- components/navigation/BurgerMenu/index.test.tsx
+++ components/navigation/BurgerMenu/index.test.tsx
@@ -1,19 +1,47 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
+
 import { BurgerMenu } from "..";
 
-describe("Burger Menu", () => {
+const setup = () => {
   const menu = ["services", "about", "contact us"];
+  const utils = render(<BurgerMenu menu={menu} />);
 
-  it("renders mobile and desktop menus", () => {
-    const { getAllByRole } = render(<BurgerMenu menu={menu} />);
+  const button = utils.getByLabelText("menu-button");
+  const modal = utils.getByLabelText("modal");
+  const modalContent = utils.getByLabelText("modal-content");
+  const navigationLists = utils.getAllByLabelText("navigation-list");
+  const navigationItems = utils.getAllByLabelText("navigation-item");
 
-    expect(getAllByRole("listitem")).toHaveLength(menu.length * 2);
+  return {
+    button,
+    modal,
+    modalContent,
+    navigationLists,
+    navigationItems,
+    ...utils,
+  };
+};
+
+describe("Burger Menu", () => {
+  it("Should render a button", () => {
+    const { button } = setup();
+
+    expect(button).toBeInTheDocument();
   });
 
-  it("render a button", () => {
-    const { getByTitle, getAllByRole } = render(<BurgerMenu menu={menu} />);
+  it("should render a modal on button click", () => {
+    const { modal, modalContent, button } = setup();
 
-    expect(getByTitle("menu")).toBeInTheDocument();
-    expect(getAllByRole("listitem")).toHaveLength(menu.length * 2);
+    expect(modal).toHaveClass("hidden");
+    fireEvent.click(button);
+    expect(modal).toHaveClass("modal");
+    expect(modalContent).toBeInTheDocument();
+  });
+
+  it("Should display a modal with menu items", () => {
+    const { navigationLists, navigationItems } = setup();
+
+    expect(navigationLists.length).toBe(2); // mobile and desktop
+    expect(navigationItems.length).toBe(6); // menu items * 2, since its mobile and desktop
   });
 });
