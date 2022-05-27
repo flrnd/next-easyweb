d9cac318776378fd637b8b10d132ad0de9275710
Florian Rand <florian.rand@gmail.com>
2022-05-27 08:25:36 +0200

    Add ariaLabel prop

--- components/layout/Grid/index.tsx
+++ components/layout/Grid/index.tsx
@@ -4,12 +4,19 @@ interface IProps {
   columns: number;
   gap?: string;
   children: ReactNode;
+  ariaLabel?: string;
 }
 
-const Grid = ({ columns, gap = "gap-3", children }: IProps): JSX.Element => {
+const Grid = ({
+  columns,
+  gap = "gap-3",
+  children,
+  ariaLabel,
+}: IProps): JSX.Element => {
   return (
     <div
       className={`inline-grid grid-cols-1 md:grid-cols-2 lg:grid-cols-${columns} ${gap}`}
+      aria-label={ariaLabel}
     >
       {children}
     </div>
