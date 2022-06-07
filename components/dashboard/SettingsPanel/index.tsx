0c3c593db2e90890a82e1cbd519f07ad9a937b99
Florian Rand <florian.rand@gmail.com>
2022-06-07 16:55:05 +0200

    fix cannot read null

--- components/dashboard/SettingsPanel/index.tsx
+++ components/dashboard/SettingsPanel/index.tsx
@@ -2,19 +2,16 @@ import { useAppSelector } from "../../../lib/hooks";
 import { Heading } from "../../typography";
 
 const SettingsPanel = (): JSX.Element => {
-  const { user, profileDetails } = useAppSelector((state) => state.user);
+  const { user } = useAppSelector((state) => state.user);
   return (
     <div className="mt-10">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="border-b-2">
           <Heading level={4} size="medium" weight="font-bold">
-            Settings
-            {user.email}
+            {`${user && user.email} settings`}
           </Heading>
         </div>
-        <div className="my-4">
-          {profileDetails.first_name} {profileDetails.last_name}
-        </div>
+        <div className="my-4"></div>
       </div>
     </div>
   );
