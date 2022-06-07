f22a7098c375834817a82b5e2da57189fd93674f
Florian Rand <florian.rand@gmail.com>
2022-06-07 12:26:44 +0200

    Add signUp logic

--- lib/features/User/index.ts
+++ lib/features/User/index.ts
@@ -1,6 +1,11 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { Session } from "@supabase/supabase-js";
-import { RootState, SignInOptions, UserState } from "../../types";
+import {
+  RootState,
+  SignInOptions,
+  SignUpOptions,
+  UserState,
+} from "../../types";
 import { supabase } from "../../util/supabase/supabase-client";
 
 const initialState: UserState = {
@@ -13,14 +18,31 @@ const initialState: UserState = {
 };
 
 export const signInUser = createAsyncThunk(
-  "users/login",
+  "users/signin",
   async (options: SignInOptions, thunkAPI) => {
     try {
       const { user, session, error } = await supabase.auth.signIn(options);
       if (error === null) {
         return { user, session };
       } else {
-        return thunkAPI.rejectWithValue(error);
+        return thunkAPI.rejectWithValue(error.message);
+      }
+    } catch (error) {
+      console.error(error);
+      thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const signUpUser = createAsyncThunk(
+  "users/signup",
+  async (options: SignUpOptions, thunkAPI) => {
+    try {
+      const { user, session, error } = await supabase.auth.signUp(options);
+      if (error === null) {
+        return { user, session };
+      } else {
+        return thunkAPI.rejectWithValue(error.message);
       }
     } catch (error) {
       console.error(error);
