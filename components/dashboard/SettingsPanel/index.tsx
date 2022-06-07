eb3f30f4da5837ae76efd35187189268676af35c
Florian Rand <florian.rand@gmail.com>
2022-06-07 16:43:31 +0200

    Placeholder data

--- components/dashboard/SettingsPanel/index.tsx
+++ components/dashboard/SettingsPanel/index.tsx
@@ -1,15 +1,20 @@
+import { useAppSelector } from "../../../lib/hooks";
 import { Heading } from "../../typography";
 
 const SettingsPanel = (): JSX.Element => {
+  const { user, profileDetails } = useAppSelector((state) => state.user);
   return (
     <div className="mt-10">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="border-b-2">
           <Heading level={4} size="medium" weight="font-bold">
             Settings
+            {user.email}
           </Heading>
         </div>
-        <div className="my-4">Settings options list</div>
+        <div className="my-4">
+          {profileDetails.first_name} {profileDetails.last_name}
+        </div>
       </div>
     </div>
   );
