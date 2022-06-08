5e12b5aedc98ed9695bcf214c8dcdd9336d55358
Florian Rand <florian.rand@gmail.com>
2022-06-08 15:28:22 +0200

    Add edit button

---
+++ components/dashboard/EditButton/index.tsx
@@ -0,0 +1,23 @@
+import { Anchor } from "../../controls";
+import { getIcon } from "../../icons";
+
+interface IProps {
+  onClick: () => void;
+}
+
+const EditButton = ({ onClick }: IProps): JSX.Element => {
+  return (
+    <div className="px-4 py-1 border-solid border-2 border-grey-100 shadow-sm hover:shadow-md rounded-md">
+      <Anchor
+        icon={getIcon("edit")}
+        size="xsmall"
+        gap="ml-1"
+        reverse={true}
+        label="Edit"
+        onClick={onClick}
+      />
+    </div>
+  );
+};
+
+export default EditButton;
