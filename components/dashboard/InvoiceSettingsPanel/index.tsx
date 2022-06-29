8741b0332e53eb591cf3bdec3fc41ed071af3813
Florian Rand <florian.rand@gmail.com>
2022-06-29 16:09:12 +0200

    Add InvoiceSettingsPanel and InvoicePanel

---
+++ components/dashboard/InvoiceSettingsPanel/index.tsx
@@ -0,0 +1,47 @@
+import router from "next/router";
+import { useEffect, useState } from "react";
+import { useAppSelector } from "../../../lib/hooks";
+import { Heading } from "../../typography";
+import EditButton from "../EditButton";
+import SaveCancelButtons from "../SaveCancelButtons";
+
+const InvoiceSettingsPanel = (): JSX.Element => {
+  const { session } = useAppSelector((state) => state.user);
+  const [edit, setEdit] = useState(false);
+
+  const handleEdit = () => setEdit(!edit);
+  const handleCancelOnClick = () => {
+    setEdit(false);
+  };
+  const handleSave = () => {
+    return "";
+  };
+
+  useEffect(() => {
+    if (!session) {
+      router.replace("/signin");
+    }
+  }, [session]);
+
+  return (
+    <div className="mt-10">
+      <div className="dashboard-panel">
+        <div className="dashboard-panel-main-section">
+          <Heading level={4} size="medium" weight="font-bold">
+            Invoice Settings
+          </Heading>
+          {!edit && <EditButton onClick={handleEdit} />}
+          {edit && (
+            <SaveCancelButtons
+              handleCancel={handleCancelOnClick}
+              handleSave={handleSave}
+            />
+          )}
+        </div>
+        <div className="dashboard-panel-form-section"></div>
+      </div>
+    </div>
+  );
+};
+
+export default InvoiceSettingsPanel;
---
+++ components/dashboard/InvoicesPanel/index.tsx
@@ -0,0 +1,28 @@
+import router from "next/router";
+import { useEffect, useState } from "react";
+import { useAppSelector } from "../../../lib/hooks";
+import { Heading } from "../../typography";
+
+const InvoicesPanel = (): JSX.Element => {
+  const { session } = useAppSelector((state) => state.user);
+
+  useEffect(() => {
+    if (!session) {
+      router.replace("/signin");
+    }
+  }, [session]);
+
+  return (
+    <div className="mt-10">
+      <div className="dashboard-panel">
+        <div className="dashboard-panel-main-section">
+          <Heading level={4} size="medium" weight="font-bold">
+            Invoices
+          </Heading>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InvoicesPanel;
