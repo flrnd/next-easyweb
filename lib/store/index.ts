df5e9fd81267b45280585e45eddda161049dc559
Florian Rand <florian.rand@gmail.com>
2023-02-03 14:07:01 +0100

    Refactor store

--- lib/store/index.ts
+++ lib/store/index.ts
@@ -1,8 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { userSlice } from "../features/User";
+import {
+  combineReducers,
+  configureStore,
+  PreloadedState,
+} from "@reduxjs/toolkit";
+import { userSlice } from "lib/features/User";
 
-export const store = configureStore({
-  reducer: {
-    user: userSlice.reducer,
-  },
+const rootReducer = combineReducers({
+  user: userSlice.reducer,
 });
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) =>
+  configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
