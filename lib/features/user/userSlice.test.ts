5e082fc48a3fbb7a258cca8d5f3cd2483c0d6309
Florian Rand <florian.rand@gmail.com>
2023-02-09 12:42:01 +0100

    wip userSlice tests

--- lib/features/user/userSlice.test.ts
+++ lib/features/user/userSlice.test.ts
@@ -1,9 +1,41 @@
-import userReducer from "lib/features/user/userSlice";
+import { setupStore } from "lib/store";
+import { signInUser } from "./userSlice";
+
+jest.mock("lib/util/supabase/supabase-client", () => ({
+  __esModule: true,
+  supabase: {
+    rpc: jest.fn(),
+  },
+}));
+
+const initialStore = setupStore();
 
 describe("userSlice", () => {
   describe("reducers", () => {
-    it("should return the initial state", () => {
-      expect(userReducer(undefined, { type: undefined })).toEqual("");
+    it("has initialState", () => {
+      const initialState = {
+        user: {
+          errorMessage: null,
+          profileDetails: null,
+          session: null,
+          siteConfig: null,
+          user: null,
+          userLoaded: false,
+        },
+      };
+
+      expect(initialStore.getState()).toEqual(initialState);
+    });
+
+    it("handles signIn", () => {
+      expect(
+        initialStore.dispatch(
+          signInUser({
+            email: "some@email.com",
+            password: "super secure pasword 12#",
+          })
+        )
+      );
     });
   });
 });
